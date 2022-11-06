012b21de4e0aef7efa360fdd0761fba481842e4e
= <mmonir.m.29@gmail.com>
2022-11-06 12:04:10 +0600

    :building_construction: Update default TokenObtainPair and TokenRefresh Serializers. Add users basic data as serializer field.

---
+++ authentication/serializers.py
@@ -0,0 +1,28 @@
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
+from rest_framework_simplejwt.exceptions import InvalidToken
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+        token['email'] = user.email
+        token['first_name'] = user.first_name
+        token['last_name'] = user.last_name
+        token['is_staff'] = user.is_staff
+        token['is_active'] = user.is_active
+        return token
+
+
+
+class MyTokenRefreshSerializer(TokenRefreshSerializer):
+    refresh = None
+
+    def validate(self, attrs):
+        attrs['refresh'] = self.context['request'].COOKIES.get('refresh_token')
+        if attrs['refresh']:
+            return super().validate(attrs)
+        raise InvalidToken("No valid Token found in cookie 'refresh_token'")
+
+
