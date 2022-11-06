b68932b655af3cec85114ba0624bb3f9b4419701
= <mmonir.m.29@gmail.com>
2022-11-06 14:04:28 +0600

    :card_file_box: Update CustomUser model. email is not unique and cannot be blank at forms.

--- authentication/models.py
+++ authentication/models.py
@@ -1,10 +1,17 @@
 
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 # Create your models here.
 
+
 class CustomUser(AbstractUser):
+    email = models.EmailField(
+        _("email address"),
+        blank=False,
+        unique=True,
+        null=False)
 
     @property
     def full_name(self):
