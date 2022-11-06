9085acfdea04eaef58e657b40e50f6416f95b7f5
= <mmonir.m.29@gmail.com>
2022-11-06 13:18:57 +0600

    :memo: Add documentation URL. Two avialable documentation redoc and swagger.

--- tms/urls.py
+++ tms/urls.py
@@ -15,8 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('schema/', SpectacularAPIView.as_view(), name='schema'),
+    path("schema/redoc/", SpectacularRedocView.as_view( url_name="schema"), name="redoc",),
+    path("schema/swagger/", SpectacularSwaggerView.as_view( url_name="schema"), name="swagger",),
 ]
