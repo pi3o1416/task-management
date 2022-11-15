54ba807d7d30f346d91777b20c46e4a2e95743f8
= <mmonir.m.29@gmail.com>
2022-11-15 13:23:28 +0600

    :sparkles: Baseic view for CustomUser Table.

---
+++ authentication/views/drf_basic_views.py
@@ -0,0 +1,69 @@
+
+from rest_framework import viewsets
+from rest_framework.response import Response
+from rest_framework import status
+from drf_spectacular.utils import extend_schema
+from ..models import CustomUser
+from ..serializers import MessageSerializer, UserSerializer
+from ..exceptions import UserGetException
+
+
+class UserViewSet(viewsets.ViewSet):
+    queryset = CustomUser.objects.all()
+
+    @extend_schema(request=UserSerializer, responses={201: UserSerializer,
+                                                      400: MessageSerializer,
+                                                      403: MessageSerializer})
+    def create(self, request):
+        """
+        Create a new User
+        """
+        breakpoint()
+        serializer = UserSerializer(data=request.POST.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.error_messages, status=status.HTTP_400_BAD_REQUEST)
+
+    @extend_schema(request=None, responses={201: UserSerializer})
+    def list(self, request):
+        """
+        Get List of User objects
+        """
+        serializer = UserSerializer(instance=self.queryset, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    @extend_schema(request=None, responses={200: UserSerializer,
+                                            400: MessageSerializer})
+    def retrieve(self, request, pk):
+        """
+        Retrieve a object detail from database
+        parameter: (int)pk
+        """
+        try:
+            user = self.get_user_object(pk)
+            serializer =  UserSerializer(instance=user)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except UserGetException as exception:
+            return Response(data={"detail": exception.args}, status=status.HTTP_400_BAD_REQUEST)
+
+#    def destroy(self, request):
+#        pass
+#
+#    def update(self, request):
+#        pass
+
+    def get_user_object(self, pk):
+        try:
+            user = CustomUser.objects.get(pk=pk)
+            return user
+        except CustomUser.DoesNotExist:
+            raise UserGetException("User with does not found")
+        except ValueError:
+            raise UserGetException("User pk whold be an Integer")
+        except Exception:
+            raise UserGetException("User object retrieve failed due to unknown causes")
+
+
+
+
