6847d1ebb0731700d10a4eafce5d07f66aa464fc
= <mmonir.m.29@gmail.com>
2022-11-15 13:28:33 +0600

    :alien: Update 'urls.py'. Add users viewset to perform basic view operations like create, list, retrieve, destroy etc.

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,11 +1,21 @@
 
-from django.urls import path
-from .views.drf_auth_views import MyTokenObtainPairView, MyTokenRefreshView, LogoutView
+from django.urls import path, include
+from rest_framework import routers
+from .views import MyTokenObtainPairView, MyTokenRefreshView, LogoutView, UserViewSet
+
+
+user_router = routers.DefaultRouter()
+user_router.register('', UserViewSet, 'user')
 
 app_name = 'authentication'
 urlpatterns = [
     path('get-token/', MyTokenObtainPairView.as_view(), name='obtain-token'),
     path('refresh-token/', MyTokenRefreshView.as_view(), name='refresh-token'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('users/', include(user_router.urls)),
 ]
+
+
+
+
 
