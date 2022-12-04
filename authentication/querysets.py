54625759b6acbd54b72d626b6b7c9715fe17cf6c
= <mmonir.m.29@gmail.com>
2022-12-04 12:45:28 +0600

    :bug: Remove unnecessary queryset methods.

--- authentication/querysets.py
+++ authentication/querysets.py
@@ -8,20 +8,17 @@ class CustomUserManager(UserManager):
     def get_queryset(self):
         return CustomUserQuerySet(self.model, using=self._db)
 
-    def all_users(self):
-        return self.get_queryset().all_users()
-
     def active_users(self):
         return self.get_queryset().active_users()
+
+    def inactive_users(self):
+        return self.get_queryset().inactive_users()
 
     def staff_users(self):
         return self.get_queryset().staff_users()
 
 
 class CustomUserQuerySet(QuerySet):
-    def all_users(self):
-        return self
-
     def active_users(self):
         return self.filter(Q(is_active=True))
 
