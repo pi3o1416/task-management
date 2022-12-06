231d55fc5c7fb0fff6f497f868a1168cc77a5304
= <mmonir.m.29@gmail.com>
2022-12-06 15:34:36 +0600

    :sparkles: Add method to get user by uidb64.

--- authentication/querysets.py
+++ authentication/querysets.py
@@ -1,5 +1,9 @@
 
+from functools import reduce
+from operator import __and__
+from django.utils.http import urlsafe_base64_decode
 from django.db.models import QuerySet
+from django.core.exceptions import ObjectDoesNotExist
 from django.db.models import Q
 
 
@@ -12,6 +16,30 @@ class CustomUserQuerySet(QuerySet):
 
     def staff_users(self):
         return self.filter(Q(is_staff=True))
+
+    def get_user(self, **kwargs):
+        try:
+            q_objects = [Q(**{key: value}) for key, value in kwargs.items()]
+            return self.filter(reduce(__and__, q_objects)).first()
+        except:
+            return None
+
+    def get_user_by_encoded_pk(self, encoded_pk):
+        try:
+            pk_bytes = urlsafe_base64_decode(encoded_pk)
+            pk = int(pk_bytes)
+            user = self.get(pk=pk)
+            return user
+        except ObjectDoesNotExist:
+            return None
+
+
+
+
+
+
+
+
 
 
 
