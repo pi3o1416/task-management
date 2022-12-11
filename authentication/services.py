ce6294ea271d61536a4397c37c54ebf0bf08e8db
= <mmonir.m.29@gmail.com>
2022-12-11 15:41:06 +0600

    :recycle: Change get_absolute_uri method.

--- authentication/services.py
+++ authentication/services.py
@@ -2,9 +2,9 @@
 from django.urls import reverse_lazy
 from django.conf import settings
 
-def get_absolute_uri(request, url_name, **kwargs):
+def get_absolute_uri(url_name, **kwargs):
     url = reverse_lazy(url_name, kwargs=kwargs)
-    absolute_uri = request.build_absolute_uri(url)
+    absolute_uri = settings.DOMAIN_URI + url
     return absolute_uri
 
 
