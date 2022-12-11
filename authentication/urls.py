7ea8a07763d79d1c1ece9db414e6f7e02239b15b
= <mmonir.m.29@gmail.com>
2022-12-11 16:01:16 +0600

    :recycle: Update active account urls.'

--- authentication/urls.py
+++ authentication/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include, re_path
 from rest_framework import routers
-from .views import MyTokenObtainPairView, MyTokenRefreshView, LogoutView, UserViewSet, ForgetPasswordView, PasswordResetView
+from .views import MyTokenObtainPairView, MyTokenRefreshView, LogoutView, UserViewSet, ForgetPasswordView, PasswordResetView, ActiveAccount
 
 
 user_router = routers.DefaultRouter()
@@ -14,7 +14,7 @@ urlpatterns = [
     path('logout/', LogoutView.as_view(), name='logout'),
     path('forget-password/', ForgetPasswordView.as_view(), name='forget-password'),
     path('reset-password/<str:uidb64>/<str:token>', PasswordResetView.as_view(), name='reset-password'),
-    path('active-account/<str:uidb64>/<str:token>', )
+    path('active-account/<str:uidb64>/<str:token>', ActiveAccount.as_view(), name='active-account'),
     path('users/', include(user_router.urls)),
 ]
 
