63be203aa5ba608b998b18c932ed57952802f6c7
= <mmonir.m.29@gmail.com>
2022-12-11 15:41:51 +0600

    :recycle: Change Email send to async.

--- authentication/models.py
+++ authentication/models.py
@@ -10,6 +10,8 @@ from django.contrib.auth.tokens import default_token_generator
 from django.utils.http import urlsafe_base64_decode, urlsafe_base64_encode
 from django.utils.translation import gettext_lazy as _
 
+from emailservice.emailclient import send_async_email
+
 from .validators import validate_aamarpay_email
 from .querysets import CustomUserManager
 from .services import get_absolute_uri
@@ -66,35 +68,33 @@ class CustomUser(AbstractUser):
     def validate_token(self, token):
         return default_token_generator.check_token(user=self, token=token)
 
-    def send_password_reset_email(self, request):
+    def send_password_reset_email(self):
         token = self.generate_token()
         uidb64 = self.generate_urlsafe_b64_encoded_pk()
-        absolute_uri = get_absolute_uri(request, 'authentication:reset-password', uidb64=uidb64, token=token)
+        absolute_uri = get_absolute_uri('authentication:reset-password', uidb64=uidb64, token=token)
         subject = 'Reset Password'
-        email_from = settings.EMAIL_HOST_USER
         email_to = [self.email]
         context = {
             'full_name': self.full_name,
             'absolute_uri': absolute_uri
         }
         html_message = render_to_string('password-reset-email.html', context=context)
-        self.send_email(subject, email_from, email_to, message=html_message)
+        send_async_email(subject=subject, email_to=email_to, message=html_message)
+        return True
 
 
-    def send_account_active_email(self, request):
+    def send_account_active_email(self):
         token = self.generate_token()
         uidb64 = self.generate_urlsafe_b64_encoded_pk()
-        absolute_uri = get_absolute_uri(request, 'authentication:active-account', uidb64=uidb64, token=token)
-        url = reverse_lazy()
-
-    def send_email(self, subject, email_from, email_to, message=""):
-        email = EmailMessage(
-            subject=subject,
-            body=message,
-            from_email=email_from,
-            to=email_to
-        )
-        email.send()
+        absolute_uri = get_absolute_uri('authentication:active-account', uidb64=uidb64, token=token)
+        subject = 'Account Activation URL'
+        email_to = [self.email]
+        context = {
+            'full_name': self.full_name,
+            'absolute_uri': absolute_uri
+        }
+        html_message = render_to_string('account-active-email.html', context=context)
+        send_async_email(subject=subject, email_to=email_to, message=html_message)
 
     def activate_account(self):
         self.is_active = True
