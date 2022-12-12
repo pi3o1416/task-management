e00c05f9999f2fc75f1bd63fb94a759851c77b0f
= <mmonir.m.29@gmail.com>
2022-12-12 12:48:09 +0600

    :sparkles: Add department models and queryset.

--- department/models.py
+++ department/models.py
@@ -1,3 +1,81 @@
-from django.db import models
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from django.contrib.auth import get_user_model
+from .querysets import DepartmentMemberQuerySet, DepartmentQuerySet, DesignationQuerySet
+
+User = get_user_model()
+
+
+class Department(models.Model):
+    name = models.CharField(
+        verbose_name=_("Department Name"),
+        max_length=200,
+        null=False,
+        unique=True,
+        blank=False,
+    )
+    description = models.TextField(
+        verbose_name=_("Department Description"),
+        null=False,
+        blank=False,
+    )
+    objects = DepartmentQuerySet.as_manager()
+
+
+class Designations(models.Model):
+    department = models.ForeignKey(
+        to=Department,
+        on_delete=models.CASCADE,
+        related_name="department_designations",
+        verbose_name=_("Department Designations")
+    )
+    title = models.CharField(
+        verbose_name=_("Designation Title"),
+        max_length=100,
+        null=False,
+        blank=False,
+    )
+    objects = DesignationQuerySet.as_manager()
+
+    class Meta:
+        unique_together = [['department', 'title']]
+
+
+class DepartmentMember(models.Model):
+    Member = models.OneToOneField(
+        to=User,
+        on_delete=models.CASCADE,
+        related_name="user_departments",
+        null=False,
+        blank=False,
+        verbose_name=_("Member")
+    )
+    department = models.ForeignKey(
+        to=Department,
+        on_delete=models.CASCADE,
+        related_name="department_members",
+        null=False,
+        blank=False,
+        verbose_name="Department"
+    )
+    Designation = models.ForeignKey(
+        to=Designations,
+        on_delete=models.RESTRICT,
+        null=False,
+        blank=False,
+        verbose_name="designated_member"
+    )
+    objects = DepartmentMemberQuerySet.as_manager()
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ department/querysets.py
@@ -0,0 +1,14 @@
+
+from django.db.models import QuerySet
+
+
+class DepartmentQuerySet(QuerySet):
+    pass
+
+
+class DesignationQuerySet(QuerySet):
+    pass
+
+
+class DepartmentMemberQuerySet(QuerySet):
+    pass
