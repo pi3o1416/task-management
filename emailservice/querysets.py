0f400b12e3a91da39189bb06f449045e07cecdb1
= <mmonir.m.29@gmail.com>
2022-12-12 12:50:24 +0600

    :construction: Add TODO to for future reference.

--- emailservice/querysets.py
+++ emailservice/querysets.py
@@ -10,6 +10,7 @@ class EmailHistoryQuerySet(QuerySet):
         return self.filter(Q(email_status=self.model.EmailStatus.FAILURE))
 
     def activate_account_emails(self):
+        #TODO: Move ACTIVE_ACCOUNT_EMAIL_SUBJECT to settings
         try:
             from authentication.services import ACTIVE_ACCOUNT_EMAIL_SUBJECT as subject
             return self.filter(Q(email_subject=subject))
@@ -17,6 +18,7 @@ class EmailHistoryQuerySet(QuerySet):
             return self.none()
 
     def password_reset_emails(self):
+        #TODO: Move PASSWORD_RESET_EMIAL_SUBJECT to settings
         try:
             from authentication.services import PASSWORD_RESET_EMIAL_SUBJECT as subject
             return self.filter(Q(email_subject=subject))
