1ab8f7ede9bdd7b2fddbdf206c655f3b575f6662
= <mmonir.m.29@gmail.com>
2022-12-12 18:09:05 +0600

    :sparkles: Add urls for department viewset.

--- department/urls.py
+++ department/urls.py
@@ -1,8 +1,13 @@
 
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from .views import DepartmentViewSet
 
+department_router = routers.DefaultRouter()
+department_router.register('', DepartmentViewSet, 'department')
 
 app_name="department"
 urlpatterns = [
+    path('', include(department_router.urls)),
 
 ]
