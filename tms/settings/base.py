a6487af2d9ebe7be43bfa8ed75c3620eee6dcdc6
= <mmonir.m.29@gmail.com>
2022-12-14 17:40:35 +0600

    Add global pagination.

--- tms/settings/base.py
+++ tms/settings/base.py
@@ -141,6 +141,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny',
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+    #Pagination
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 2,
 }
 
 
