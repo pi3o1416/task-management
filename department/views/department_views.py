99b5d86fbfc0157cc1a22f11c67569b94729ba3b
= <mmonir.m.29@gmail.com>
2022-12-14 17:39:53 +0600

    Add department update retrieve destroy view.

--- department/views/department_views.py
+++ department/views/department_views.py
@@ -2,10 +2,11 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import status
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 from drf_spectacular.utils import extend_schema
 from ..models import Department
-from ..serializers import DepartmentSerializer, FieldErrorsSerializer
+from ..serializers import DepartmentSerializer, FieldErrorsSerializer, MessageSerializer
+from ..exceptions import DepartmentGetException
 
 
 class DepartmentViewSet(ViewSet):
@@ -19,24 +20,51 @@ class DepartmentViewSet(ViewSet):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(responses={200: DepartmentSerializer})
     def list(self, request):
+        print(request.data)
         serializer_class = self.get_serializer_class()
         departments = Department.objects.all()
         serializer = serializer_class(instance=departments, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-#    def update(self, request, pk):
-#        pass
-#
-#    def destroy(self, request, pk):
-#        pass
-#
-#    def retrieve(self, request, pk):
-#        pass
-#
+    @extend_schema(request=DepartmentSerializer, responses={202: DepartmentSerializer,
+                                                            400: FieldErrorsSerializer,
+                                                            404: MessageSerializer})
+    def update(self, request, pk):
+        try:
+            department = Department.objects.get(pk=pk)
+            serializer_class = self.get_serializer_class()
+            serializer = serializer_class(instance=department, data=request.data)
+            if serializer.is_valid():
+                serializer.update()
+                return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except DepartmentGetException as exception:
+            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+
+    @extend_schema(responses={200: MessageSerializer,
+                              404: MessageSerializer})
+    def destroy(self, request, pk):
+        try:
+            department = Department.objects.get_department(pk=pk)
+            department.delete()
+            return Response(data={"detail": ("Department delete successful",)}, status=status.HTTP_200_OK)
+        except DepartmentGetException as exception:
+            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+
+    @extend_schema(responses={200: DepartmentSerializer,
+                              404: MessageSerializer})
+    def retrieve(self, request, pk):
+        try:
+            serializer_class = self.get_serializer_class()
+            department = Department.objects.get_department(pk=pk)
+            serializer = serializer_class(instance=department)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except DepartmentGetException as exception:
+            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+
     def get_serializer_class(self):
-        if self.action == 'create':
-            return DepartmentSerializer
         return DepartmentSerializer
 
 
