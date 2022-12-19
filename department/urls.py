ee478b082a1fc95bda09bfa70dad5e0424ee0260
= <mmonir.m.29@gmail.com>
2022-12-19 18:16:53 +0600

    Add designation router urls with urlpatterns.

--- department/urls.py
+++ department/urls.py
@@ -2,12 +2,16 @@
 from django.urls import path, include
 from rest_framework import routers
 from .views import DepartmentViewSet
+from .views import DesignationViewSet
 
 department_router = routers.DefaultRouter()
 department_router.register('', DepartmentViewSet, 'department')
 
+designation_router = routers.DefaultRouter()
+designation_router.register('', DesignationViewSet, 'designation')
+
 app_name="department"
 urlpatterns = [
+    path('designations/', include(designation_router.urls)),
     path('', include(department_router.urls)),
-
 ]
