f394ba2ce095f9936835edc6ef1df15980646db7
= <mmonir.m.29@gmail.com>
2022-12-19 11:24:03 +0600

    Add production settings.

--- tms/settings/production.py
+++ tms/settings/production.py
@@ -1,10 +1,62 @@
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
 from .base import *
+from . import CONFIG
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['tmsv2.aamarpay.com']
+
+# Email Backend
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST_USER = CONFIG['EMAIL_HOST_USER']
+
+# Domain
+DOMAIN_URI = "https://tmsv2.aamarpay.com"
+
+# Celery Configuration Options
+CELERY_TIMEZONE = "UTC"
+CELERY_TASK_TRACK_STARTED = True
+CELERY_TASK_TIME_LIMIT = 30 * 60
+CELERY_RESULT_BACKEND = 'django-db'
+CELERY_CACHE_BACKEND = 'django-cache'
+CELERY_RESULT_EXTENDED = True
+CELERY_BROKER_URL = 'amqp://user:bitnami@tmsv2_rabbitmq:5672/'
+
+#Configuration for sentry
+sentry_sdk.init(
+    dsn="https://a1ff75fa9a95483693c31dea9957c4f3@o1182560.ingest.sentry.io/4504327056850944",
+    integrations=[
+        DjangoIntegration(),
+    ],
+
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production.
+    traces_sample_rate=1.0,
+
+    # If you wish to associate users to errors (assuming you are using
+    # django.contrib.auth) you may enable sending PII data.
+    send_default_pii=True
+)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
