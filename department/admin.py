3a972da4634608c5e75c36d27ba7ff5e429bd26f
= <mmonir.m.29@gmail.com>
2022-12-19 18:12:48 +0600

    Add Department, Designation, DepartmentMember into admin.

--- department/admin.py
+++ department/admin.py
@@ -1,3 +1,25 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Designations, Department, DepartmentMember
+
+
+@admin.register(Designations)
+class DesignationsAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(DepartmentMember)
+class DepartmentMemberAdmin(admin.ModelAdmin):
+    pass
+
+
+
+
+
+
+
