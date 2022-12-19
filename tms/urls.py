c86bc734430144e223166f3350f8106352b841e3
= <mmonir.m.29@gmail.com>
2022-12-19 12:30:28 +0600

    Add static and media file urls.

--- tms/urls.py
+++ tms/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
@@ -25,3 +27,10 @@ urlpatterns = [
     path("schema/redoc/", SpectacularRedocView.as_view( url_name="schema"), name="redoc",),
     path("schema/swagger/", SpectacularSwaggerView.as_view( url_name="schema"), name="swagger",),
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
+
+
