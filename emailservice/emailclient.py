1143561d0f448673bf5b8aa8eaf6eb05a69bcdac
= <mmonir.m.29@gmail.com>
2022-12-20 15:12:25 +0600

    Change delay to send task.

--- emailservice/emailclient.py
+++ emailservice/emailclient.py
@@ -1,9 +1,17 @@
 
 from .tasks import add, send_email
+from tms.celery import app
 
 
 def send_async_email(subject, email_to, message):
-    return send_email.delay(subject, email_to, message)
+    temp = {
+        "subject": subject,
+        "email_to": email_to,
+        "message": message
+    }
+    app.send_task("send_email", kwargs=temp)
+    return True
+#    return send_email.delay(subject, email_to, message)
 
 
 
