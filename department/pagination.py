40162a077bb2e7260543a43d0cd072fe3ea5578c
= <mmonir.m.29@gmail.com>
2022-12-20 12:29:38 +0600

    Remove Small Medium and Large pagination and add one custom pagination for handeling all pagination.

--- department/pagination.py
+++ department/pagination.py
@@ -1,19 +1,14 @@
 
 from rest_framework.pagination import PageNumberPagination
 
-class SmallPageNumberPagination(PageNumberPagination):
-    page_size = 1000
+class CustomPageNumberPagination(PageNumberPagination):
+    page_size = 10
     page_size_query_param = 'page_size'
 
 
-class MediumPageNumberPagination(PageNumberPagination):
-    page_size = 1000
-    page_size_query_param = 'page_size'
 
 
-class LargePageNumberPagination(PageNumberPagination):
-    page_size = 1000
-    page_size_query_param = 'page_size'
+
 
 
 
