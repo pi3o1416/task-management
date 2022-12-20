08a221c0d58e799c313d7fc0e118e4128ba6dc51
= <mmonir.m.29@gmail.com>
2022-12-20 12:33:15 +0600

    Add destroy, update, retrieve view and pagination with list view.

--- department/views/designation_views.py
+++ department/views/designation_views.py
@@ -1,14 +1,15 @@
 
 from rest_framework.viewsets import ViewSet
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 from rest_framework import status
-from ..pagination import SmallPageNumberPagination, MediumPageNumberPagination, LargePageNumberPagination
-from drf_spectacular.utils import extend_schema
-from ..serializers import DesignationSerializer
+from drf_spectacular.utils import extend_schema, OpenApiParameter
+from ..exceptions import DesignationGetException
+from ..pagination import CustomPageNumberPagination
+from ..serializers import DesignationSerializer, MessageSerializer, FieldErrorsSerializer, DesignationPaginatedSerializer
+from ..models import Designations
 
 
-class DesignationViewSet(ViewSet, PageNumberPagination):
+class DesignationViewSet(ViewSet, CustomPageNumberPagination):
     @extend_schema(request=DesignationSerializer, responses={})
     def create(self, request):
         """
@@ -21,21 +22,70 @@ class DesignationViewSet(ViewSet, PageNumberPagination):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
+    @extend_schema(responses={200: DesignationPaginatedSerializer},
+                   parameters=[OpenApiParameter(name='page', type=int),
+                               OpenApiParameter(name='page_size', type=int)])
     def list(self, request):
         """
-        List of Department Designation
+        List of All Designation.
         """
         serializer_class = self.get_serializer_class()
         designations = Designations.objects.all()
         page = self.paginate_queryset(queryset=designations, request=request)
         serializer = serializer_class(instance=page, many=True)
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return self.get_paginated_response(serializer.data)
+
+    @extend_schema(responses={200: DesignationSerializer,
+                              404: MessageSerializer})
+    def destroy(self, request, pk):
+        """
+        Destroy Department
+        URL Parameter: Designation Primary Key(int)
+        """
+        try:
+            designation = Designations.objects.get_designation(pk=pk)
+            designation.delete()
+            return Response(data={"detail": ("Designation delete successful",)}, status=status.HTTP_200_OK)
+        except DesignationGetException as exception:
+            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+
+    @extend_schema(responses={200: DesignationSerializer,
+                              404: MessageSerializer})
+    def retrieve(self, request, pk):
+        """
+        Retrieve Designation Object
+        URL Parameter: Designation Primary Key(int)
+        """
+        try:
+            designation = Designations.objects.get_designation(pk=pk)
+            serializer_class = self.get_serializer_class()
+            serializer = serializer_class(instance=designation)
+            return Response(data=serializer.data, status=status.HTTP_200_OK)
+        except DesignationGetException as exception:
+            return Response(data=exception.args, status=status.HTTP_404_NOT_FOUND)
+
+    @extend_schema(request=DesignationSerializer, responses={202: DesignationSerializer,
+                                                             400: FieldErrorsSerializer,
+                                                             404: MessageSerializer})
+    def update(self, request, pk):
+        """
+        Update Designation Object
+        URL Parameter: Designation Primary Key(int)
+        """
+        try:
+            designation = Designations.objects.get_designation(pk=pk)
+            serializer_class = self.get_serializer_class()
+            serializer = serializer_class(instance=designation, data=request.data)
+            if serializer.is_valid():
+                serializer.update()
+                return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        except DesignationGetException as exception:
+            return Response(data=exception.args, status=status.HTTP_404_NOT_FOUND)
+
 
     def get_serializer_class(self):
         return DesignationSerializer
-
-    def get_pagination_class(self):
-        return SmallPageNumberPagination
 
 
 
