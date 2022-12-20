63a632e349f18479704744952d1b88091dfa3bb8
= <mmonir.m.29@gmail.com>
2022-12-20 12:32:14 +0600

    Add update method with DesignationSerializer and Create New DesignationPaginatedSerializer to send paginated response.

--- department/serializers/designation_serializers.py
+++ department/serializers/designation_serializers.py
@@ -6,25 +6,22 @@ class DesignationSerializer(serializers.ModelSerializer):
     class Meta:
         model = Designations
         fields = ['pk', 'department', 'title']
+        read_only_fields = ['pk']
+
+    def update(self):
+        assert self.validated_data, "Call is_valid method before update"
+        assert self.instance, "Initialize seirlaizer with department instance"
+        self.instance.update(**self.validated_data)
 
 
-class DesignationListSerializer(serializers.Serializer):
-    results = DesignationSerializer()
-    current_page = serializers.IntegerField()
-    total_page = serializers.IntegerField()
-    page_limit = serializers.IntegerField()
+class DesignationPaginatedSerializer(serializers.Serializer):
+    count = serializers.IntegerField()
+    next = serializers.URLField()
+    previous = serializers.URLField()
+    results = DesignationSerializer(many=True)
 
-    def __init__(self, paginator, current_page_no, **kwargs):
-        data = self.preprocess_page_data(paginator, current_page_no)
-        print(data)
-        super().__init__(data=data, **kwargs)
 
-    def preprocess_page_data(self, page, current_page_no):
-        data = {
-            'results': list(page.get_page(current_page_no)),
-            'current_page': current_page_no,
-            'total_page': page.num_pages,
-            'page_limit': page.per_page
-        }
-        return data
+
+
+
 
