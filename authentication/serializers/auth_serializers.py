26d0d9326c83f174ba3c4ef6e59a7589ca898158
= <mmonir.m.29@gmail.com>
2022-12-21 12:41:15 +0600

    Create AccessTokenSerializer for documentations.

--- authentication/serializers/auth_serializers.py
+++ authentication/serializers/auth_serializers.py
@@ -1,9 +1,11 @@
 from django.utils.translation import gettext_lazy as _
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 from rest_framework_simplejwt.exceptions import InvalidToken
+from rest_framework import serializers
 
 
 class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+
     @classmethod
     def get_token(cls, user):
         token = super().get_token(user)
@@ -23,4 +25,32 @@ class MyTokenRefreshSerializer(TokenRefreshSerializer):
         if attrs['refresh']:
             return super().validate(attrs)
         raise InvalidToken("No valid Token found in cookie 'refresh_token'")
+
+
+class AccessTokenSerializer(serializers.Serializer):
+    """
+    Only for documentations
+    """
+    access = serializers.CharField()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
