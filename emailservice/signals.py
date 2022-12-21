c1562f5741bd02cc27a1e9945f5c95d074ea81a5
= <mmonir.m.29@gmail.com>
2022-12-21 11:43:38 +0600

    Update create_new_history signal. instance task_kwargs are now send to get_args_from_task_kwargs for mining.

--- emailservice/signals.py
+++ emailservice/signals.py
@@ -3,13 +3,14 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save
 from django_celery_results.models import TaskResult
 from .models import EmailHistory
-from .services import get_args_from_task_args
+from .services import get_args_from_task_kwargs
 
 
 #@receiver(signal=post_save, sender=TaskResult)
 #def create_new_history(sender, instance, created, **kwargs):
+#    print('triggered')
 #    if created:
-#        email_subject, email_body, email_to = get_args_from_task_args(instance.task_args)
+#        email_subject, email_body, email_to = get_args_from_task_args(instance.task_kwargs)
 #        for email in email_to:
 #            email = EmailHistory.email_manager.create(
 #                email_task=instance,
