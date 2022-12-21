ab404f71a078399adc3efcbb19a2c87513f24242
= <mmonir.m.29@gmail.com>
2022-12-21 11:41:56 +0600

    Update get_args_from_task_kwargs. arguments are now preprocessed from task kwargs.

--- emailservice/services.py
+++ emailservice/services.py
@@ -1,9 +1,10 @@
 
 import re
 
-def get_args_from_task_args(task_args):
-    splited_task_args = re.findall("'(.+?)'", task_args)
-    email_subject = splited_task_args[0]
-    email_body = splited_task_args[-1]
-    email_to = splited_task_args[1:-1]
+def get_args_from_task_kwargs(task_kwargs):
+    splited_task_args = re.findall("{(.+?)}", task_kwargs)[0]
+    splited_task_args = re.findall("'(.+?)'", splited_task_args)
+    email_subject = splited_task_args[1]
+    email_body = splited_task_args[5]
+    email_to = splited_task_args[3]
     return email_subject, email_body, email_to
