b749db985f61a5cc0ccd7e40eb43253a72910099
= <mmonir.m.29@gmail.com>
2022-12-21 18:16:13 +0600

    Create signal to update department_name, designation_title, member_full_name before save.

---
+++ department/signals.py
@@ -0,0 +1,20 @@
+
+from django.dispatch import receiver
+from django.db.models.signals import pre_save
+from .models import DepartmentMember
+
+
+@receiver(signal=pre_save, sender=DepartmentMember)
+def fill_department_designation_user_info(sender, instance, **kwargs):
+    instance.department_name = instance.department.name
+    instance.member_full_name = instance.member.full_name
+    instance.designation_title = instance.designation.title
+    return instance
+
+
+
+
+
+
+
+
