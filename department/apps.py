30e38f0e31c5b9eb0eacc2fe766761a1480901dd
= <mmonir.m.29@gmail.com>
2022-12-21 18:16:44 +0600

    Add signals.py in app ready.

--- department/apps.py
+++ department/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DepartmentConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'department'
+
+    def ready(self):
+        from . import signals
