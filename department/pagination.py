8ef66ce5ef79f27ca76c4ef163b0a6a8f2b9926d
= <mmonir.m.29@gmail.com>
2022-12-21 15:16:51 +0600

    override paginate_queryset method to format InvalidPage exception response.

--- department/pagination.py
+++ department/pagination.py
@@ -1,9 +1,40 @@
 
+from django.utils.translation import gettext_lazy as _
 from rest_framework.pagination import PageNumberPagination
+from rest_framework.pagination import InvalidPage, NotFound
 
 class CustomPageNumberPagination(PageNumberPagination):
     page_size = 10
     page_size_query_param = 'page_size'
+
+    def paginate_queryset(self, queryset, request, view=None):
+        """
+        Paginate a queryset if required, either returning a
+        page object, or `None` if pagination is not configured for this view.
+        """
+        page_size = self.get_page_size(request)
+        if not page_size:
+            return None
+
+        paginator = self.django_paginator_class(queryset, page_size)
+        page_number = self.get_page_number(request, paginator)
+
+        try:
+            self.page = paginator.page(page_number)
+        except InvalidPage as exc:
+            msg = self.invalid_page_message.format(
+                page_number=page_number, message=str(exc)
+            )
+            raise NotFound({"detail": [msg]})
+
+        if paginator.num_pages > 1 and self.template is not None:
+            # The browsable API should display pagination controls.
+            self.display_page_controls = True
+
+        self.request = request
+        return list(self.page)
+
+
 
 
 
