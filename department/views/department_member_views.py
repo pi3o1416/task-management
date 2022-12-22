afdfb23bcfe967bd1479eddeaa54a98c41bb1f02
= <mmonir.m.29@gmail.com>
2022-12-22 15:31:32 +0600

    Change hard coded view documentations. Instead import them from documentations module.

--- department/views/department_member_views.py
+++ department/views/department_member_views.py
@@ -1,18 +1,21 @@
 
-from drf_spectacular.utils import extend_schema, OpenApiParameter
+from drf_spectacular.utils import extend_schema
 from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
 from ..models import DepartmentMember
-from ..serializers import DepartmentMemberSerializer, FieldErrorsSerializer, MessageSerializer, DepartmentMemberPaginatedSerializer, DepartmentMemberUpdateSerializer
+from ..serializers import DepartmentMemberSerializer, DepartmentMemberUpdateSerializer
 from ..pagination import CustomPageNumberPagination
+from ..documentations import department_member_docs as docs
 
 
 class DepartmentMemberViewSet(ViewSet, CustomPageNumberPagination):
 
-    @extend_schema(responses={201: DepartmentMemberSerializer,
-                              400: MessageSerializer})
+    @extend_schema(
+        responses=docs.DepartmentMemberCreateDoc.responses,
+        parameters=docs.DepartmentMemberCreateDoc.parameters
+    )
     def create(self, request):
         serializer_class = self.get_serializer_class()
         serializer = serializer_class(data=request.data)
@@ -21,15 +24,10 @@ class DepartmentMemberViewSet(ViewSet, CustomPageNumberPagination):
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    @extend_schema(responses={200: DepartmentMemberPaginatedSerializer},
-                   parameters=[OpenApiParameter(name='page', type=int),
-                               OpenApiParameter(name='page_size', type=int),
-                               OpenApiParameter(name='department_name', type=str),
-                               OpenApiParameter(name='designation_title', type=str),
-                               OpenApiParameter(name='member_full_name', type=str),
-                               OpenApiParameter(name='department', type=int),
-                               OpenApiParameter(name='member', type=int),
-                               OpenApiParameter(name='designation', type=int)])
+    @extend_schema(
+        responses=docs.DepartmentMemberListDoc.responses,
+        parameters=docs.DepartmentMemberListDoc.parameters
+    )
     def list(self, request):
         serializer_class = self.get_serializer_class()
         members = DepartmentMember.objects.filter_from_query_prams(request)
@@ -37,24 +35,29 @@ class DepartmentMemberViewSet(ViewSet, CustomPageNumberPagination):
         serializer = serializer_class(instance=page, many=True)
         return self.get_paginated_response(data=serializer.data)
 
-    @extend_schema(responses={200: DepartmentMemberSerializer,
-                              404: MessageSerializer})
+    @extend_schema(
+        responses=docs.DepartmentMemberRetrieve.responses,
+        parameters=docs.DepartmentMemberRetrieve.parameters
+    )
     def retrieve(self, request, pk):
         serializer_class = self.get_serializer_class()
         member = DepartmentMember.objects.get_member(pk=pk)
         serializer = serializer_class(instance=member)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    @extend_schema(responses={202: MessageSerializer,
-                              404: MessageSerializer})
+    @extend_schema(
+        responses=docs.DepartmentMemberDestroyDoc.responses,
+        parameters=docs.DepartmentMemberDestroyDoc.parameters
+    )
     def destroy(self, request, pk):
         member = DepartmentMember.objects.get_member(pk=pk)
         member.delete()
         return Response(data={"detail": ["Department Member Delete Successful"]}, status=status.HTTP_202_ACCEPTED)
 
-    @extend_schema(responses={202: DepartmentMemberSerializer,
-                              400: FieldErrorsSerializer,
-                              404: MessageSerializer})
+    @extend_schema(
+        responses=docs.DepartmentMemberUpdateDoc.responses,
+        parameters=docs.DepartmentMemberUpdateDoc.parameters
+    )
     def update(self, request, pk):
         member = DepartmentMember.objects.get_member(pk=pk)
         serializer_class = self.get_serializer_class()
@@ -72,13 +75,10 @@ class DepartmentMemberViewSet(ViewSet, CustomPageNumberPagination):
 
 class MembersOfDepartmentView(APIView, CustomPageNumberPagination):
 
-    @extend_schema(responses={200: DepartmentMemberPaginatedSerializer},
-                   parameters=[OpenApiParameter(name='page', type=int),
-                               OpenApiParameter(name='page_size', type=int),
-                               OpenApiParameter(name='designation_title', type=str),
-                               OpenApiParameter(name='member_full_name', type=str),
-                               OpenApiParameter(name='member', type=int),
-                               OpenApiParameter(name='designation', type=int)])
+    @extend_schema(
+        responses=docs.MembersOfDepartmentDoc.responses,
+        parameters=docs.MembersOfDepartmentDoc.parameters
+    )
     def get(self, request, department_pk):
         department_members = DepartmentMember.objects.\
             get_members_of_department(department_pk=department_pk).\
