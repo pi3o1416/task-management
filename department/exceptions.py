85d8f9b2b3697c0241c99574f5e44bb8f8f5c285
= <mmonir.m.29@gmail.com>
2022-12-22 17:53:39 +0600

    Add RestrictedDeletionError to handle database entity deletion restriction error.

--- department/exceptions.py
+++ department/exceptions.py
@@ -1,4 +1,15 @@
 
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class RestrictedDeletionError(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = _('Instance deletion restricted due to foreign key restriction')
+    default_code = 'restricted'
+
+
 class DepartmentGetException(Exception):
     pass
 
@@ -9,6 +20,7 @@ class DesignationGetException(Exception):
 
 class DepartmentMemberGetException(Exception):
     pass
+
 
 
 
