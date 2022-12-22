9e06e8d827a275370be246fc6b72ef4b126ee2e3
= <mmonir.m.29@gmail.com>
2022-12-22 15:21:20 +0600

    Create Seperate Documentation for DepartmentMember Model Views.

---
+++ department/documentations/__init__.py
@@ -0,0 +1,4 @@
+
+from .department_docs import *
+from .department_member_docs import *
+from .designation_docs import *
---
+++ department/documentations/department_member_docs.py
@@ -0,0 +1,72 @@
+
+from drf_spectacular.utils import OpenApiParameter
+from ..serializers import (
+    DepartmentMemberSerializer, FieldErrorsSerializer,
+    MessageSerializer, DepartmentMemberPaginatedSerializer,
+)
+
+from collections import namedtuple
+
+Documentation = namedtuple(
+    "Documentation",
+    ["request", "responses", "parameters"],
+    defaults=[None, None, None]
+)
+
+DepartmentMemberCreateDoc = Documentation(
+    responses={
+        201: DepartmentMemberSerializer,
+        400: MessageSerializer
+    }
+)
+
+DepartmentMemberListDoc = Documentation(
+    responses={
+        200: DepartmentMemberPaginatedSerializer
+    },
+    parameters=[
+        OpenApiParameter(name='page', type=int),
+        OpenApiParameter(name='page_size', type=int),
+        OpenApiParameter(name='department_name', type=str),
+        OpenApiParameter(name='designation_title', type=str),
+        OpenApiParameter(name='member_full_name', type=str),
+        OpenApiParameter(name='department', type=int),
+        OpenApiParameter(name='member', type=int),
+        OpenApiParameter(name='designation', type=int)
+    ]
+)
+
+DepartmentMemberUpdateDoc = Documentation(
+    responses={
+        202: DepartmentMemberSerializer,
+        400: FieldErrorsSerializer,
+        404: MessageSerializer}
+)
+
+DepartmentMemberDestroyDoc = Documentation(
+    responses={
+        202: MessageSerializer,
+        404: MessageSerializer
+    }
+)
+
+DepartmentMemberRetrieve = Documentation(
+    responses={
+        200: DepartmentMemberSerializer,
+        404: MessageSerializer
+    }
+)
+
+MembersOfDepartmentViewDoc = Documentation(
+    responses={
+        200: DepartmentMemberPaginatedSerializer
+    },
+    parameters=[
+        OpenApiParameter(name='page', type=int),
+        OpenApiParameter(name='page_size', type=int),
+        OpenApiParameter(name='designation_title', type=str),
+        OpenApiParameter(name='member_full_name', type=str),
+        OpenApiParameter(name='member', type=int),
+        OpenApiParameter(name='designation', type=int)
+    ]
+)
