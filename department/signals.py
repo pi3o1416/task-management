a73e597a4550dc932bf005970ffa95ac63b66452
= <mmonir.m.29@gmail.com>
2022-12-29 16:19:25 +0600

    Add signal to restrict member department and designation department are same.

--- department/signals.py
+++ department/signals.py
@@ -1,4 +1,5 @@
 
+from rest_framework.exceptions import NotAcceptable
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
 from .models import DepartmentMember
@@ -10,6 +11,15 @@ def fill_department_designation_user_info(sender, instance, **kwargs):
     instance.member_full_name = instance.member.full_name
     instance.designation_title = instance.designation.title
     return instance
+
+@receiver(signal=pre_save, sender=DepartmentMember)
+def test_designation_from_same_department(sender, instance, **kwargs):
+    department = instance.department
+    designation_department = instance.designation.department
+    if department != designation_department:
+        raise NotAcceptable({"detail": ["Member department and department designation did not match."]})
+
+
 
 
 
