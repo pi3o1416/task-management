c85efabfccf27a5fea484aa3acddf8ccada2e516
= <mmonir.m.29@gmail.com>
2022-12-29 16:15:21 +0600

    Add department head model.

--- department/models.py
+++ department/models.py
@@ -150,6 +150,23 @@ class DepartmentMember(models.Model):
         return "{}".format(self.member.first_name)
 
 
+class DepartmentHead(models.Model):
+    department = models.OneToOneField(
+        to=Department,
+        on_delete=models.CASCADE,
+        related_name='department_head',
+        verbose_name=_("Department Head")
+    )
+    head = models.ForeignKey(
+        to=DepartmentMember,
+        on_delete=models.RESTRICT,
+        related_name='head_of_department',
+        verbose_name=_("Head Of Department")
+    )
+
+    def __str__(self):
+        return self.department.name
+
 
 
 
