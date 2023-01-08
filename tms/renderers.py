f3e62b8b8bf66732767f71b6de37f265a81aa263
= <mmonir.m.29@gmail.com>
2023-01-08 10:47:52 +0600

    Create new JSON renderer for custom json response.

---
+++ tms/renderers.py
@@ -0,0 +1,26 @@
+
+from rest_framework.renderers import JSONRenderer
+
+
+class CustomJSONRenderer(JSONRenderer):
+    def render(self, data, accepted_media_type=None, renderer_context=None):
+        custom_data = {
+            "data": None,
+            "error": None,
+            "status": None,
+        }
+        response = renderer_context.get("response")
+        custom_data["status"] = response.status_code
+        if response.status_code >= 400:
+            custom_data["error"] = data
+        else:
+            custom_data["data"] = data
+        print(data.get("data"))
+        print(data.get("status"))
+        print(data.get("message"))
+        return super().render(custom_data, accepted_media_type, renderer_context)
+
+
+
+
+
--- tms/settings/base.py
+++ tms/settings/base.py
@@ -142,6 +142,11 @@ AUTH_USER_MODEL = 'authentication.CustomUser'
 REST_FRAMEWORK = {
     # Use Django's standard `django.contrib.auth` permissions,
     # or allow read-only access for unauthenticated users.
+    'DEFAULT_RENDERER_CLASSES': [
+        'tms.renderers.CustomJSONRenderer',
+        'rest_framework.renderers.JSONRenderer',
+        'rest_framework.renderers.BrowsableAPIRenderer',
+    ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ],
