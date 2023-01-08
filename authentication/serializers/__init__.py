29f7ee391340feab81484c1fa00ec87dd1e45628
= <mmonir.m.29@gmail.com>
2023-01-08 16:32:14 +0600

    Add New Response serializer

--- authentication/serializers/__init__.py
+++ authentication/serializers/__init__.py
@@ -1,4 +1,5 @@
 
+from rest_framework import serializers
 from .auth_serializers import *
 from .basic_serializers import *
 
@@ -9,6 +10,16 @@ class MessageSerializer(serializers.Serializer):
     Only used for documentations
     """
     detail = serializers.ListField()
+
+class ErrorResponse(serializers.Serializer):
+    detail = serializers.ListField()
+    field_errors = serializers.JSONField()
+
+
+class ResponseSerializer(serializers.Serializer):
+    status=serializers.IntegerField()
+    data=serializers.JSONField()
+    error=ErrorResponse()
 
 
 
