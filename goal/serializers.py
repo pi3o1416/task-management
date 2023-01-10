66666c2c92b3e3ed3dff631de41721c1819933e3
= <mmonir.m.29@gmail.com>
2023-01-10 18:30:32 +0600

    Add GoalReviewSerializer and EmptySerializer.

--- goal/serializers.py
+++ goal/serializers.py
@@ -4,11 +4,29 @@ from rest_framework import serializers
 from .models import Goal
 
 
+class EmptySerializer(serializers.Serializer):
+    pass
+
 class GoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goal
         fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
         read_only_fields = ['pk', 'review_status', 'review', 'completion']
+
+
+class GoalReviewSerializer(serializers.Serializer):
+    class Meta:
+        model=Goal
+        fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
+        read_only_fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'completion']
+
+    
+    def add_review(self, instance, validated_data):
+        review = self.validated_data.get('review')
+        instance.add_reivew(review)
+        return True
+
+
 
 
 
