efb5820601ef9a6946a5479ecad61759e1f7f337
= <mmonir.m.29@gmail.com>
2023-01-10 13:09:43 +0600

    Add 'department' on goal list_display.

--- goal/admin.py
+++ goal/admin.py
@@ -5,7 +5,7 @@ from .models import Goal
 
 @admin.register(Goal)
 class GloadAdmin(admin.ModelAdmin):
-    list_display = ['title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
+    list_display = ['department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
 
 
 
