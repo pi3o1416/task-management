5957551b5c72aea2d8a77255656c82aa8a0f669b
= <mmonir.m.29@gmail.com>
2023-01-10 18:28:44 +0600

    Add filter_from_query_params, get_goal_by_pk, mtehod. and _generate_q_objects_from_query_params function.

--- goal/querysets.py
+++ goal/querysets.py
@@ -1,6 +1,10 @@
 
+from django.db.models import QuerySet, Q, CharField
+from django.utils.translation import gettext_lazy as _
+from functools import reduce
 from operator import __and__
-from django.db.models import QuerySet, Q
+from rest_framework.request import Request
+from rest_framework.exceptions import APIException, NotFound
 
 
 class GoalQuerySet(QuerySet):
@@ -13,5 +17,37 @@ class GoalQuerySet(QuerySet):
     def get_department_yearly_plan(self, department, year:int):
         department_yearly_goals = self.filter(department=department, year=year)
         return department_yearly_goals
+
+    def filter_from_query_params(self, request: Request):
+        try:
+            q_objects = _generate_q_objects_from_query_params(self.model, request)
+            if q_objects:
+                return self.filter(reduce(__and__, q_objects))
+            return self.all()
+        except Exception as exception:
+            raise APIException(detail={"detail": exception.args})
+
+    def get_goal_by_pk(self, pk):
+        try:
+            goal = self.get(pk=pk)
+            return goal
+        except self.model.DoesNotExist:
+            raise NotFound(detail={"detail": (_("Goal with pk={} does not exist."),)})
+        except ValueError:
+            raise NotFound(detail={"detail": [_("Goal primary key should be an integer.")]})
+        except Exception as exception:
+            raise NotFound(detail={"detail": exception.args})
+
+def _generate_q_objects_from_query_params(Model, request: Request) -> list:
+    query_params = request.query_params
+    fields = {field.name: field for field in Model._meta.fields}
+    q_objects = []
+    for param, value in query_params.items():
+        if param in fields.keys():
+            if isinstance(fields[param], CharField):
+                q_objects.append(Q(('{}__icontains'.format(param), value)))
+            else:
+                q_objects.append(Q((param, value)))
+    return q_objects
 
 
