156e37379d4eb07782381fe6d0cfb83dcf80273e
= <mmonir.m.29@gmail.com>
2023-01-10 15:16:06 +0600

    Create GoalViewSet with create operation.

--- goal/views.py
+++ goal/views.py
@@ -1,3 +1,27 @@
-from django.shortcuts import render
 
-# Create your views here.
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.viewsets import ViewSet
+
+from .models import Goal
+from .serializers import GoalSerializer
+
+
+class GoalViewSet(ViewSet):
+    def create(self, request):
+        serializer = self.get_serializer_class()(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response({"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_serializer_class(self):
+        return GoalSerializer
+
+
+
+
+
+
+
+
