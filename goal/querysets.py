611ec76127f6e97192cc84eb2fac4660ab3b5ad5
= <mmonir.m.29@gmail.com>
2023-01-10 15:28:37 +0600

    get_department_quarter_plan quarter is now str within [1, 2, 3, 4].

--- goal/querysets.py
+++ goal/querysets.py
@@ -4,16 +4,10 @@ from django.db.models import QuerySet, Q
 
 
 class GoalQuerySet(QuerySet):
-    def get_department_quarter_plan(self, department, year:int, quarter:int):
-        assert quarter in [1, 2, 3, 4], "Quarter should be an integer within {1, 2, 3, 4}"
+    def get_department_quarter_plan(self, department, year:int, quarter:str):
+        assert quarter in ["1", "2", "3", "4"], "Quarter should be an integer within {1, 2, 3, 4}"
         yearly_goals = self.get_department_yearly_plan(department=department, year=year)
-        quarters = {
-            1: self.model.QuarterChoices.QUARTER1,
-            2: self.model.QuarterChoices.QUARTER2,
-            3: self.model.QuarterChoices.QUARTER3,
-            4: self.model.QuarterChoices.QUARTER4
-        }
-        quarterly_goals = yearly_goals.filter(Q(quarter=quarters[quarter]))
+        quarterly_goals = yearly_goals.filter(Q(quarter=quarter))
         return quarterly_goals
 
     def get_department_yearly_plan(self, department, year:int):
