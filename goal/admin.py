6769e6a7db372cf8ce64b566883cc49ffc9fa14e
= <mmonir.m.29@gmail.com>
2023-01-10 13:04:01 +0600

    Add admin panel for Goal model.

--- goal/admin.py
+++ goal/admin.py
@@ -1,3 +1,17 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Goal
+
+
+@admin.register(Goal)
+class GloadAdmin(admin.ModelAdmin):
+    list_display = ['title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
+
+
+
+
+
+
+
+
+
