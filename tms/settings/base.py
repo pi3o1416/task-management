d995374028a7e41ecb7d7861406059a6abfde97d
= <mmonir.m.29@gmail.com>
2023-01-10 18:31:56 +0600

    Update rest framework pagination page size to 10.

--- tms/settings/base.py
+++ tms/settings/base.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     #Pagination
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 2,
+    'PAGE_SIZE': 10,
 }
 
 
