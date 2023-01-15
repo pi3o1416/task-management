59d1d5e44113473f9e5c9cf960a04dcfcf7f6974
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-15 22:07:03 +0600

    Add DepartmentGoal View in url.

--- goal/urls.py
+++ goal/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import GoalViewSet
+from .views import GoalViewSet, DepartmentGoals
 
 goal_router = DefaultRouter()
 goal_router.register('', GoalViewSet, 'goal')
@@ -10,4 +10,5 @@ goal_router.register('', GoalViewSet, 'goal')
 app_name='goal'
 urlpatterns = [
     path('', include(goal_router.urls)),
+    path('department-goals/<int:department_pk>/', DepartmentGoals.as_view(), name="department_goals"),
 ]
