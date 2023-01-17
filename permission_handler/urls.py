10379c9fa8a7a0e22e20538e3b748d7c2901d856
= <mmonir.m.29@gmail.com>
2023-01-17 12:48:17 +0600

    Add GroupViewSet urls to app urls.

--- permission_handler/urls.py
+++ permission_handler/urls.py
@@ -1,16 +1,19 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PermissionViewSet
+from .views import PermissionViewSet, GroupViewSet
 
 
 permission_router = DefaultRouter()
 permission_router.register('', PermissionViewSet, 'permission')
 
+group_router = DefaultRouter()
+group_router.register('', GroupViewSet, 'group')
+
 app_name='permission_handler'
 urlpatterns = [
-    path('permission/', include(permission_router.urls)),
-
+    path('permissions/', include(permission_router.urls)),
+    path('groups/', include(group_router.urls)),
 ]
 
 
