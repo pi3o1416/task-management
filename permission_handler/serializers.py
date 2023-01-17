df47e88e4a9fb04ea28a6f31124fd88f40eba877
= <mmonir.m.29@gmail.com>
2023-01-17 18:04:58 +0600

    Create GroupAssignSerializer to assign group to user.

--- permission_handler/serializers.py
+++ permission_handler/serializers.py
@@ -1,7 +1,10 @@
 
+from authentication.models import CustomUser
 from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
+from rest_framework.exceptions import ValidationError
 
 
 class ContentTypeSerializer(serializers.ModelSerializer):
@@ -43,6 +46,34 @@ class GroupDetailSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
         fields = '__all__'
+
+
+class GroupAssignSerializer(serializers.Serializer):
+    user = serializers.IntegerField()
+    group = serializers.IntegerField()
+
+    def validate_user(self, value):
+        user = CustomUser.objects.filter(pk=value).first()
+        if user != None:
+            return user
+        raise ValidationError(_("User with pk={} does not exist".format(value)))
+
+    def validate_group(self, value):
+        group = Group.objects.filter(pk=value).first()
+        if group != None:
+            return group
+        raise ValidationError(_("Group with pk={} does not exist".format(value)))
+
+    def save(self):
+        assert self.validated_data != None, "Validate serializer assign group"
+        breakpoint()
+        group = self.validated_data.get('group')
+        user = self.validated_data.get('user')
+        group.user_set.add(user.pk)
+        return True
+
+
+
 
 
 
