c913b425c5286087a1eec3608dc46624ee0cdf26
= <mmonir.m.29@gmail.com>
2023-01-17 14:22:31 +0600

    Add view doc string.

--- permission_handler/views.py
+++ permission_handler/views.py
@@ -12,6 +12,9 @@ from .queries import get_group_by_pk, get_permission_by_pk
 
 class GroupViewSet(ViewSet):
     def create(self, request):
+        """
+        Group create view
+        """
         serializer = self.get_serializer_class()(data=request.data)
         if serializer.is_valid():
             serializer.save()
@@ -19,21 +22,33 @@ class GroupViewSet(ViewSet):
         return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request):
+        """
+        Group list view
+        """
         groups = Group.objects.all()
         serializer = self.get_serializer_class()(instance=groups, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk):
+        """
+        Group destroy view
+        """
         group = get_group_by_pk(pk)
         group.delete()
         return Response(data={"detail": [_("Group delete successful")]}, status=status.HTTP_202_ACCEPTED)
 
     def retrieve(self, request, pk):
+        """
+        Group retrieve view
+        """
         group = get_group_by_pk(pk)
         serializer = self.get_serializer_class()(instance=group)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, pk):
+        """
+        Group udpate view
+        """
         group = get_group_by_pk(pk)
         serializer = self.get_serializer_class()(data=request.data, instance=group)
         if serializer.is_valid():
@@ -42,6 +57,9 @@ class GroupViewSet(ViewSet):
         return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
+        """
+        Get serializer class by action
+        """
         if self.action in ['retrieve', 'list']:
             return GroupDetailSerializer
         else:
@@ -50,16 +68,25 @@ class GroupViewSet(ViewSet):
 
 class PermissionViewSet(ViewSet):
     def list(self, request):
+        """
+        Permission list view
+        """
         permissions = Permission.objects.all()
         serializer = self.get_serializer_class()(instance=permissions, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def retrieve(self, request, pk):
+        """
+        Permission retrieve view
+        """
         permission = get_permission_by_pk(pk)
         serializer = self.get_serializer_class()(instance=permission)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def get_serializer_class(self):
+        """
+        Get serializer class by action
+        """
         if self.action in ['retrieve']:
             return PermissionDetailSerializer
         else:
