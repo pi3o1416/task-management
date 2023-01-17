689b2cca5e7967f2cef3c4b371bdeeb5379ff4d9
= <mmonir.m.29@gmail.com>
2023-01-17 10:55:56 +0600

    Create PermissionViewSet with list view method.

--- permission_handler/views.py
+++ permission_handler/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
 
-# Create your views here.
+from django.contrib.auth.models import Permission, Group
+from rest_framework.viewsets import ViewSet
+from rest_framework.response import Response
+from rest_framework import status
+
+from .serializers import PermissionSerializer
+
+
+class GroupViewSet(ViewSet):
+    pass
+
+
+class PermissionViewSet(ViewSet):
+    def list(self, request):
+        permissions = Permission.objects.all()
+        serializer = self.get_serializer_class()(instance=permissions, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
+
+    def get_serializer_class(self):
+        return PermissionSerializer
+
