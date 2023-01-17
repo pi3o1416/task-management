18c26ec7079d782868f3b44a76fed6760d2cccb0
= <mmonir.m.29@gmail.com>
2023-01-17 12:47:53 +0600

    Add GroupSerializer.

--- permission_handler/serializers.py
+++ permission_handler/serializers.py
@@ -1,5 +1,5 @@
 
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Permission, Group
 from django.contrib.contenttypes.models import ContentType
 from rest_framework import serializers
 
@@ -17,6 +17,15 @@ class PermissionSerializer(serializers.ModelSerializer):
         model = Permission
         fields = ['pk', 'name', 'content_type', 'codename']
         read_only_fields = ['pk', 'name', 'content_type', 'codename']
+
+
+class GroupSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Group
+        fields = '__all__'
+
+
+
 
 
 
