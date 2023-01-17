69e6a1584ba590b92f955318e73d3ed63a7bf797
= <mmonir.m.29@gmail.com>
2023-01-17 18:08:05 +0600

    Add AssignGroup and AllUserPermissions view urls.

--- permission_handler/urls.py
+++ permission_handler/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PermissionViewSet, GroupViewSet
+from .views import PermissionViewSet, GroupViewSet, AssignGroup, AllUserPermissions
 
 
 permission_router = DefaultRouter()
@@ -14,6 +14,8 @@ app_name='permission_handler'
 urlpatterns = [
     path('permissions/', include(permission_router.urls)),
     path('groups/', include(group_router.urls)),
+    path('assign-group/', AssignGroup.as_view(), name='assign-group'),
+    path('user-permissions/<int:user_pk>/', AllUserPermissions.as_view(), name='user-permissions'),
 ]
 
 
