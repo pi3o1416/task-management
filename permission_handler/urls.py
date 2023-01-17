b48c48220071136c05420a9ffd266e41fa07ab53
= <mmonir.m.29@gmail.com>
2023-01-17 10:56:46 +0600

    Add PermissionViewSet to app urls.

---
+++ permission_handler/urls.py
@@ -0,0 +1,19 @@
+
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import PermissionViewSet
+
+
+permission_router = DefaultRouter()
+permission_router.register('', PermissionViewSet, 'permission')
+
+app_name='permission_handler'
+urlpatterns = [
+    path('', include(permission_router.urls)),
+]
+
+
+
+
+
+
