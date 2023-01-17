d54791d7e2b21f53c58ff3062f97c5c0300a8a4a
= <mmonir.m.29@gmail.com>
2023-01-17 12:55:22 +0600

    Add get_permission_by_pk function to handle permission get with exception handling.

--- permission_handler/queries.py
+++ permission_handler/queries.py
@@ -1,6 +1,6 @@
 
 from operator import __and__
-from django.contrib.auth.models import Group
+from django.contrib.auth.models import Group, Permission
 from django.utils.translation import gettext_lazy as _
 from rest_framework.exceptions import NotFound
 
@@ -14,6 +14,18 @@ def get_group_by_pk(pk):
         raise NotFound({"detail": [_("Group pk should be an integer")]})
     except Exception as exception:
         raise NotFound({"detail": exception.args})
+
+def get_permission_by_pk(pk):
+    try:
+        permission = Permission.objects.get(pk=pk)
+        return permission
+    except Permission.DoesNotExist:
+        raise NotFound({"detail": [_("Permission with pk={} does not exist".format(pk))]})
+    except ValueError:
+        raise NotFound({"detail": [_("Permission pk should be integer")]})
+    except Exception as exception:
+        raise NotFound({"detail": exception.args})
+
 
 
 
