5a39ff229c17607a8124389e3c21e84473a92285
= <mmonir.m.29@gmail.com>
2023-01-18 16:06:53 +0600

    Add UserTaskAdmin and TaskTreeAdmin class for model admin support.

--- task/admin.py
+++ task/admin.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 
-from .models import TaskAttachments, Task
+from .models import TaskAttachments, Task, UsersTasks, TaskTree
 
 
 @admin.register(Task)
@@ -12,6 +12,16 @@ class TaskAdmin(admin.ModelAdmin):
 @admin.register(TaskAttachments)
 class TaskAttachmentsAdmin(admin.ModelAdmin):
     list_display = [field.name for field in TaskAttachments._meta.fields]
+
+
+@admin.register(UsersTasks)
+class UsersTasksAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in UsersTasks._meta.fields]
+
+
+@admin.register(TaskTree)
+class TaskTreeAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in TaskTree._meta.fields]
 
 
 
