466acb114beca32a0995070552dd4d439b22a2f8
= <mmonir.m.29@gmail.com>
2023-01-18 13:41:41 +0600

    Add admin panel for Task and TaskAttachments model.

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,20 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import TaskAttachments, Task
+
+
+@admin.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Task._meta.fields]
+
+
+@admin.register(TaskAttachments)
+class TaskAttachmentsAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in TaskAttachments._meta.fields]
+
+
+
+
+
+
