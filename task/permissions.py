39bb4577bad4eb660bb08157b8399cf0c0f3624d
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-22 19:40:45 +0600

    Add IsOwner Permission class to verify task owner.

--- task/permissions.py
+++ task/permissions.py
@@ -1,11 +1,20 @@
 
 from django.contrib.auth import get_user_model
 from rest_framework.permissions import BasePermission
+from .models import Task
 
 class CanViewAllTasks(BasePermission):
     def has_permission(self, request, view):
         user = request.user
         if user.has_perm('task.can_view_all_tasks'):
+            return True
+        return False
+
+
+class IsOwner(BasePermission):
+    def has_object_permission(self, request, view, obj:Task):
+        user = request.user
+        if obj.created_by == user:
             return True
         return False
 
