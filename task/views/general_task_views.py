84cbde09925cf23dc9356448732f8b45e706ba70
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-22 13:02:34 +0600

    :bug: Fix bug on task instance creation. serializer.save replaced by serializer.create and created_by field updated through update_task_owner model method. Add permission class.

--- task/views/general_task_views.py
+++ task/views/general_task_views.py
@@ -2,18 +2,19 @@
 from rest_framework.viewsets import ViewSet
 from rest_framework import status
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.pagination import PageNumberPagination
 
 from ..serializers import TaskSerializer
-from ..models import Task
 
 
-class TaskViewSet(ViewSet):
+class TaskViewSet(ViewSet, PageNumberPagination):
     def create(self, request):
         user = request.user
         serializer = self.get_serializer_class()(data=request.data)
         if serializer.is_valid():
-            task = serializer.save(commit=False)
-            task.created_by = user
+            task = serializer.create(commit=False)
+            task.update_task_owner(user, commit=False)
             task.save()
             return Response(data=serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
@@ -21,6 +22,13 @@ class TaskViewSet(ViewSet):
 
     def get_serializer_class(self):
         return TaskSerializer
+
+    def get_permissions(self):
+        permission_classes = [IsAuthenticated]
+        if self.action == 'create':
+            permission_classes += []
+        return [permission() for permission in permission_classes]
+
 
 
 
