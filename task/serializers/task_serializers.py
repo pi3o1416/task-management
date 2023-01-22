9c07c91316884df7f01d013e336464b18904908b
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-22 12:59:16 +0600

    Overwrite create method of TaskSerializer. instance commit is now based on method parameter commit.

--- task/serializers/task_serializers.py
+++ task/serializers/task_serializers.py
@@ -11,6 +11,13 @@ class TaskSerializer(serializers.ModelSerializer):
         read_only_fields = ['created_by', 'created_at', 'created_by_user_username',
                             'created_by_user_fullname', 'approved_by_dept_head', 'status']
 
+    def create(self, commit=True):
+        assert self.validated_data != None, "Validated serialzier before create object"
+        task = Task(**self.validated_data)
+        if commit == True:
+            task.save()
+        return task
+
 
 
 
