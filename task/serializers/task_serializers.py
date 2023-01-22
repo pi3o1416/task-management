37510a6cd6287a2a9aa9e4925850630dd6167e57
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-22 19:41:56 +0600

    Add update method to TaskSerializer.

--- task/serializers/task_serializers.py
+++ task/serializers/task_serializers.py
@@ -7,8 +7,10 @@ from ..models import Task, TaskTree
 class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
-        fields = '__all__'
-        read_only_fields = ['created_by', 'created_at', 'created_by_user_username',
+        fields = ['pk', 'created_by', 'title', 'description', 'created_at', 'last_date',
+                  'approved_by_dept_head', 'status', 'priority', 'created_by_user_username',
+                  'created_by_user_fullname']
+        read_only_fields = ['pk', 'created_by', 'created_at', 'created_by_user_username',
                             'created_by_user_fullname', 'approved_by_dept_head', 'status']
 
     def create(self, commit=True):
@@ -18,6 +20,13 @@ class TaskSerializer(serializers.ModelSerializer):
             task.save()
         return task
 
-
-
-
+#    def update(self, instance, commit = True):
+#        assert self.validated_data != None, "Validate serializer before update"
+#        assert type(instance) is Task, "Should be an instance of Task model"
+#        instance.title = self.validated_data["title"]
+#        instance.description = self.validated_data["description"]
+#        instance.last_date = self.validated_data["last_date"]
+#        instance.priority = self.validated_data["priority"]
+#        if commit:
+#            instance.save(update_fields=['title', 'description', 'status', 'last_date'])
+#        return instance
