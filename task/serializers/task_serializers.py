107d9cb400145549afe0a78beb08d09b9cc168b7
Monir Hossain <mmonir.m.29@gmail.com>
2023-01-22 19:52:33 +0600

    :bug: Uncomment accidental comment of update method. replace approved_my_department_head by approval_status.

--- task/serializers/task_serializers.py
+++ task/serializers/task_serializers.py
@@ -8,10 +8,10 @@ class TaskSerializer(serializers.ModelSerializer):
     class Meta:
         model = Task
         fields = ['pk', 'created_by', 'title', 'description', 'created_at', 'last_date',
-                  'approved_by_dept_head', 'status', 'priority', 'created_by_user_username',
+                  'approval_status', 'status', 'priority', 'created_by_user_username',
                   'created_by_user_fullname']
         read_only_fields = ['pk', 'created_by', 'created_at', 'created_by_user_username',
-                            'created_by_user_fullname', 'approved_by_dept_head', 'status']
+                            'created_by_user_fullname', 'approval_status', 'status']
 
     def create(self, commit=True):
         assert self.validated_data != None, "Validated serialzier before create object"
@@ -20,13 +20,13 @@ class TaskSerializer(serializers.ModelSerializer):
             task.save()
         return task
 
-#    def update(self, instance, commit = True):
-#        assert self.validated_data != None, "Validate serializer before update"
-#        assert type(instance) is Task, "Should be an instance of Task model"
-#        instance.title = self.validated_data["title"]
-#        instance.description = self.validated_data["description"]
-#        instance.last_date = self.validated_data["last_date"]
-#        instance.priority = self.validated_data["priority"]
-#        if commit:
-#            instance.save(update_fields=['title', 'description', 'status', 'last_date'])
-#        return instance
+    def update(self, instance, commit = True):
+        assert self.validated_data != None, "Validate serializer before update"
+        assert type(instance) is Task, "Should be an instance of Task model"
+        instance.title = self.validated_data["title"]
+        instance.description = self.validated_data["description"]
+        instance.last_date = self.validated_data["last_date"]
+        instance.priority = self.validated_data["priority"]
+        if commit:
+            instance.save(update_fields=['title', 'description', 'status', 'last_date'])
+        return instance
