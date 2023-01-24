3d35ec56705cd2675c68bfa37a8454a2139d8e2a
= <mmonir.m.29@gmail.com>
2023-01-24 18:04:33 +0600

    Add UsersTasksCreateAndAssignSerializer and UsersDetailSerializer for detail view.

--- task/serializers/users_tasks_serializers.py
+++ task/serializers/users_tasks_serializers.py
@@ -1,13 +1,73 @@
 
+from django.contrib.auth import get_user_model
+from django.utils.translation import gettext_lazy as _
 from rest_framework import serializers
-from ..models import UsersTasks
+
+from task.exceptions import DBOperationFailed
+from ..models import UsersTasks, Task
+from ..exceptions import DBOperationFailed
+from .task_serializers import TaskSerializer
+
+
+User = get_user_model()
 
 
 class UsersTasksSerializers(serializers.ModelSerializer):
     class Meta:
         model = UsersTasks
         fields = '__all__'
-        read_only_fields = ['assigned_to', 'user_username', 'user_full_name']
+        read_only_fields = ['user_username', 'user_full_name']
+
+
+class UsersTasksDetailSerializer(serializers.ModelSerializer):
+    task = TaskSerializer()
+    class Meta:
+        model = UsersTasks
+        fields = '__all__'
+        read_only_fields = ['assigned_to', 'task', 'user_username', 'user_full_name']
+
+
+class UsersTasksCreateAndAssignSerializer(serializers.ModelSerializer):
+    task = TaskSerializer()
+    assigned_to = serializers.IntegerField()
+    class Meta:
+        model = UsersTasks
+        fields = ['task', 'assigned_to']
+
+    def create(self, validated_data:dict, user):
+        try:
+            task_data = validated_data.pop('task')
+            assigned_to = validated_data.pop('assigned_to')
+            task = self._create_task(task_data=task_data, user=user)
+            self._assign_task(task, assigned_to)
+            return True
+        except:
+            pass
+
+    def _create_task(self, task_data:dict, user):
+        try:
+            task = Task.create_factory(commit=False, **task_data)
+            task.created_by = user
+            task.save()
+        except Exception as exception:
+            raise DBOperationFailed(detail={"detail": _(exception.__str__())})
+
+    def _assign_task(self, task, assigned_to_pk):
+        try:
+            assigned_to_user = User.objects.get_user_by_pk(assigned_to_pk)
+            users_task = UsersTasks.create_factory(commit=False, task=task, assigned_to=assigned_to_user)
+            users_task.save()
+        except Exception as exception:
+            raise DBOperationFailed(detail={"detail": _(exception.__str__())})
+
+
+
+
+
+
+
+
+
 
 
 
