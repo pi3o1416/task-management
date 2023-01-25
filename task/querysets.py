ac07c7336fcf521be3c8c3b390feb1ed7a875554
= <mmonir.m.29@gmail.com>
2023-01-25 14:30:04 +0600

    Add filter_with_task queryset method with UsersTasks QuerySet to filter with task field.

--- task/querysets.py
+++ task/querysets.py
@@ -2,7 +2,7 @@
 from functools import reduce
 from operator import __and__
 from django.utils.translation import gettext_lazy as _
-from django.db.models import QuerySet, CharField, Q
+from django.db.models import QuerySet, CharField, Q, TextField
 from rest_framework.request import Request
 from rest_framework.exceptions import APIException, NotFound
 
@@ -42,6 +42,25 @@ class UsersTasksQuerySet(QuerySet):
            return self.all()
        except Exception as exception:
            raise APIException(detail={"detail": exception.args})
+
+    def filter_with_task(self, request: Request):
+        query_params = request.query_params
+        task_model = self.model._meta.get_field('task').remote_field.model
+        fields = {field.name: field for field in task_model._meta.fields}
+        q_objects = []
+        for param, value in query_params.items():
+            if param in fields.keys():
+                if isinstance(fields[param], CharField) or isinstance(fields[param], TextField):
+                    q_objects.append(Q(('task__{}__icontains'.format(param), value)))
+        return self.filter_with_q_objects(q_objects=q_objects)
+
+    def filter_with_q_objects(self, q_objects):
+        try:
+            if q_objects:
+                return self.filter(reduce(__and__, q_objects))
+            return self.all()
+        except Exception as exception:
+            raise APIException(detail={"detail": exception.__str__()})
 
     def get_user_task_by_pk(self, pk):
         try:
