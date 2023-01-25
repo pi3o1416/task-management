436ce08e4b37b992a879f940cd5c0c8eef887edc
= <mmonir.m.29@gmail.com>
2023-01-25 14:31:08 +0600

    Add UserTasksList View to url.

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, UsersTasksViewSet, UsersTasksCreateAndAssign
+from .views import TaskViewSet, UsersTasksViewSet, UsersTasksCreateAndAssign, UserTasksList
 
 
 task_router = DefaultRouter()
@@ -14,6 +14,7 @@ users_tasks_router.register('', UsersTasksViewSet, 'users-tasks')
 app_name='task'
 urlpatterns = [
     path('users-tasks/', include(users_tasks_router.urls)),
+    path('user/<int:user_pk>/', UserTasksList.as_view(), name='user-tasks-list'),
     path('user-task-create/', UsersTasksCreateAndAssign.as_view(), name='user_task_create'),
     path('', include(task_router.urls)),
 ]
