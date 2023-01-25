bdc4c6bf55d187807b8424a3ec4659cec32b317e
= <mmonir.m.29@gmail.com>
2023-01-25 11:42:57 +0600

    :bug: Fix UsersTasksCreateAndAssign Serializer error. task instance is not return from _create_task method and _assign_task instance return UsersTasks instance. finally UsersTasks instance return to view.

--- task/serializers/users_tasks_serializers.py
+++ task/serializers/users_tasks_serializers.py
@@ -34,29 +34,31 @@ class UsersTasksCreateAndAssignSerializer(serializers.ModelSerializer):
         model = UsersTasks
         fields = ['task', 'assigned_to']
 
-    def create(self, validated_data:dict, user):
+    def create(self, validated_data, user):
         try:
             task_data = validated_data.pop('task')
             assigned_to = validated_data.pop('assigned_to')
             task = self._create_task(task_data=task_data, user=user)
-            self._assign_task(task, assigned_to)
-            return True
-        except:
-            pass
+            user_task = self._assign_task(task, assigned_to)
+            return user_task
+        except Exception as exception:
+            raise DBOperationFailed(detail={"detail": _(exception.__str__())})
 
-    def _create_task(self, task_data:dict, user):
+    def _create_task(self, task_data, user):
         try:
             task = Task.create_factory(commit=False, **task_data)
             task.created_by = user
             task.save()
+            return task
         except Exception as exception:
             raise DBOperationFailed(detail={"detail": _(exception.__str__())})
 
     def _assign_task(self, task, assigned_to_pk):
         try:
             assigned_to_user = User.objects.get_user_by_pk(assigned_to_pk)
-            users_task = UsersTasks.create_factory(commit=False, task=task, assigned_to=assigned_to_user)
-            users_task.save()
+            user_task = UsersTasks.create_factory(commit=False, task=task, assigned_to=assigned_to_user)
+            user_task.save()
+            return user_task
         except Exception as exception:
             raise DBOperationFailed(detail={"detail": _(exception.__str__())})
 
