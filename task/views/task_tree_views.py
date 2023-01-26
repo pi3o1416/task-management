c154724ad31812d0744509f15cddbb65d0076863
= <mmonir.m.29@gmail.com>
2023-01-26 12:46:13 +0600

    Update CreateSubTask APIView. update view due to serializer change.

--- task/views/task_tree_views.py
+++ task/views/task_tree_views.py
@@ -1,26 +1,34 @@
 
 from django.utils.translation import gettext_lazy as _
-from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
-from ..serializers import TaskTreeCreateSerializer, TaskTreeDetailSerializer
+from ..serializers import SubTaskCreateSerializer, TaskTreeDetailSerializer
+from ..models import Task
 
 
-class CreateSubtusk(APIView):
-    serializer_class = TaskTreeCreateSerializer
+class CreateSubTask(APIView):
+    serializer_class = SubTaskCreateSerializer
     permission_classes = [IsAuthenticated]
 
-    def post(self, request):
+    def post(self, request, task_pk):
         user = request.user
+        parent_task = self.get_object(pk=task_pk)
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
-            task_tree = serializer.create(user=user)
+            child_task = serializer.create(user=user, commit=True)
+            task_tree = serializer.create_task_tree(child=child_task, parent=parent_task, commit=True)
             detail_serializer = TaskTreeDetailSerializer(instance=task_tree)
             return Response(data=detail_serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_object(self, pk):
+        task = Task.objects.get_task_by_pk(pk=pk)
+        return task
+
+
 
 
 
