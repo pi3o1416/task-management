c98b82fa6c469c58e91ffd38d15dcee9c645f39c
= <mmonir.m.29@gmail.com>
2023-01-26 14:50:00 +0600

    Add GetSubTasks APIView to url.

--- task/urls.py
+++ task/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import TaskViewSet, UsersTasksViewSet, UsersTasksCreateAndAssign, UserTasksList, TaskAttachmentsAdd, TaskAttachmentsDelete, TaskBasedAttachments, CreateSubTask
+from .views import TaskViewSet, UsersTasksViewSet, UsersTasksCreateAndAssign, UserTasksList, TaskAttachmentsAdd, TaskAttachmentsDelete, TaskBasedAttachments, CreateSubTask, GetSubTasks
 
 
 task_router = DefaultRouter()
@@ -17,6 +17,7 @@ urlpatterns = [
     path('user/<int:user_pk>/', UserTasksList.as_view(), name='user-tasks-list'),
     path('user-task-create/', UsersTasksCreateAndAssign.as_view(), name='user_task_create'),
     path('<int:task_pk>/create-subtask/', CreateSubTask.as_view(), name='create-sub-task'),
+    path('<int:task_pk>/subtasks/', GetSubTasks.as_view(), name='get-subtasks'),
     path('<int:task_pk>/add-attachment/', TaskAttachmentsAdd.as_view(), name='attach-file'),
     path('<int:task_pk>/attachments/', TaskBasedAttachments.as_view(), name='task-based-attachments'),
     path('delete-attachment/<int:task_attachment_pk>/', TaskAttachmentsDelete.as_view(), name='delete-attachment'),
