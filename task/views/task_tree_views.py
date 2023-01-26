2d5c81a9d584b2526ee30e8828adf122ac3fb351
= <mmonir.m.29@gmail.com>
2023-01-26 14:45:09 +0600

    Add New APIView GetSubTasks to get list of subtask of a parent task.

--- task/views/task_tree_views.py
+++ task/views/task_tree_views.py
@@ -5,7 +5,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
 from rest_framework import status
 
-from ..serializers import SubTaskCreateSerializer, TaskTreeDetailSerializer
+from ..serializers import SubTaskCreateSerializer, TaskTreeDetailSerializer, TaskSerializer
 from ..models import Task
 
 
@@ -23,6 +23,20 @@ class CreateSubTask(APIView):
             detail_serializer = TaskTreeDetailSerializer(instance=task_tree)
             return Response(data=detail_serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    def get_object(self, pk):
+        task = Task.objects.get_task_by_pk(pk=pk)
+        return task
+
+
+class GetSubTasks(APIView):
+    serializer_class = TaskSerializer
+
+    def get(self, request, task_pk):
+        task = self.get_object(pk=task_pk)
+        subtasks = Task.objects.get_subtasks(parent_task=task)
+        serializer = self.serializer_class(instance=subtasks, many=True)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def get_object(self, pk):
         task = Task.objects.get_task_by_pk(pk=pk)
