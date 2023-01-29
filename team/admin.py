187bea8a69cd92dd7f245a0c460ab28091bd126f
= <mmonir.m.29@gmail.com>
2023-01-29 17:55:13 +0600

    Add Admin panel for team model.

--- team/admin.py
+++ team/admin.py
@@ -1,3 +1,8 @@
-from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+from .models import Team
+
+
+@admin.register(Team)
+class TeamAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'description', 'team_lead', 'department']
