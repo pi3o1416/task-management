9ab1f065ff5816da2327fb9b9eac5dfc781693dd
= <mmonir.m.29@gmail.com>
2023-01-30 13:07:43 +0600

    Add filter_from_query_params queryset method for TeamQuerySet.

--- team/querysets.py
+++ team/querysets.py
@@ -1,7 +1,13 @@
 
+from functools import reduce
+from operator import __and__
 from django.db.models import QuerySet
 from django.utils.translation import gettext_lazy as _
-from rest_framework.exceptions import NotFound
+from rest_framework.exceptions import NotFound, APIException
+from rest_framework.request import Request
+
+from services.query_services import generate_q_objects_from_query_params
+
 
 
 class TeamQuerySet(QuerySet):
@@ -14,3 +20,15 @@ class TeamQuerySet(QuerySet):
             raise NotFound(detail=_("Team pk should be integer"))
         except Exception as exception:
             raise NotFound(detail=_(exception.__str__()))
+
+    def filter_from_query_params(self, request: Request):
+       try:
+           q_objects = generate_q_objects_from_query_params(self.model, request)
+           if q_objects:
+               return self.filter(reduce(__and__, q_objects))
+           return self.all()
+       except Exception as exception:
+           raise APIException(detail={"detail": exception.args})
+
+
+
