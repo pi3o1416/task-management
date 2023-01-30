07ebf2c9cec1911857fbb997179d18ecc2af8daa
= <mmonir.m.29@gmail.com>
2023-01-30 18:25:10 +0600

    Update fill_team_lead_info signal function due to database field change.

--- team/signals.py
+++ team/signals.py
@@ -1,16 +1,16 @@
 
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from .models import Team
 
 
-@receiver(signal=pre_save, sender=Team)
-def fill_team_lead_info(sender, instance, created, **kwarge):
+@receiver(signal=post_save, sender=Team)
+def fill_team_lead_info(sender, instance, created, **kwargs):
     if created:
         team_lead = instance.team_lead
-        instance.team_lead_username = team_lead.username
-        instance.team_lead_first_name = team_lead.first_name
-        instance.team_lead_last_name = team_lead.last_name
-        return instance
+        instance.team_lead_full_name = team_lead.member_full_name
+        instance.save()
+
+
 
 
