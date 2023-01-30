9b8f41a7958de6c06de37fb7a8eeaf0a8c37ae05
= <mmonir.m.29@gmail.com>
2023-01-30 18:31:12 +0600

    Remove unnecessery import from serializer.

--- team/views/team_views.py
+++ team/views/team_views.py
@@ -6,7 +6,7 @@ from rest_framework.response import Response
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
-from ..serializers import TeamSerializer, TeamUpdateSerializer, TeamLeadChangeSerializer
+from ..serializers import TeamSerializer, TeamUpdateSerializer
 from ..models import Team
 
 
