f334ee33e1082d91f89d677f02d4eccc186838dd
= <mmonir.m.29@gmail.com>
2023-01-30 13:35:19 +0600

    Add exceptions to services module to use throught the project.

---
+++ services/exceptions.py
@@ -0,0 +1,19 @@
+
+from django.utils.translation import gettext_lazy as _
+from rest_framework.exceptions import APIException
+from rest_framework import status
+
+
+class DBOperationFailed(APIException):
+    status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
+    default_detail = _("Database operation failed")
+    default_code = 'db_operation_failed'
+
+
+class InvalidRequest(APIException):
+    status_code = status.HTTP_409_CONFLICT
+    default_detail = _("Invalid request, does not match with current state")
+    default_code = 'invalid_request'
+
+
+
