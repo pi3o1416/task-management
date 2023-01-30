09260ae16eee8182f70367d6963d8b1bc4444925
= <mmonir.m.29@gmail.com>
2023-01-30 12:51:27 +0600

    Include team app urls to project urls.

--- tms/urls.py
+++ tms/urls.py
@@ -26,6 +26,7 @@ urlpatterns = [
     path('goals/', include('goal.urls')),
     path('authorization/', include('permission_handler.urls')),
     path('tasks/', include('task.urls')),
+    path('team/', include('team.urls')),
     path('schema/', SpectacularAPIView.as_view(), name='schema'),
     path("schema/redoc/", SpectacularRedocView.as_view( url_name="schema"), name="redoc",),
     path("schema/swagger/", SpectacularSwaggerView.as_view( url_name="schema"), name="swagger",),
