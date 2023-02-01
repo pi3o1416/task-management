bd40aa028ffac8e72b5bbd1cfb23fd5cf5409794
= <mmonir.m.29@gmail.com>
2023-02-01 12:56:03 +0600

    Add fill_team_member_info signal. A signal to fill team member full_name and username.

--- team/signals.py
+++ team/signals.py
@@ -1,7 +1,7 @@
 
 from django.dispatch import receiver
 from django.db.models.signals import pre_save
-from .models import Team
+from .models import Team, TeamMember
 
 
 @receiver(signal=pre_save, sender=Team)
@@ -18,6 +18,15 @@ def update_team_lead_info(sender, instance, update_fields, **kwargs):
     if update_fields and 'team_lead' in update_fields:
         team_lead = instance.team_lead
         instance.team_lead_full_name = team_lead.member_full_name
+    return instance
+
+
+@receiver(signal=pre_save, sender=TeamMember)
+def fill_team_member_info(sender, instance, update_fields, **kwargs):
+    if not update_fields:
+        member = instance.member
+        instance.member_full_name = member.full_name
+        instance.member_username = member.username
     return instance
 
 
