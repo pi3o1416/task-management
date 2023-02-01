bfc5abe180387b97bd8d387e25903c7dd6472acd
= <mmonir.m.29@gmail.com>
2023-02-01 18:12:47 +0600

    Add add_team_lead_as_team_member signal to add team lead as team member.

--- team/signals.py
+++ team/signals.py
@@ -1,6 +1,6 @@
 
 from django.dispatch import receiver
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from .models import Team, TeamMember
 
 
@@ -28,6 +28,15 @@ def fill_team_member_info(sender, instance, update_fields, **kwargs):
         instance.member_full_name = member.full_name
         instance.member_username = member.username
     return instance
+
+
+
+@receiver(signal=post_save, sender=Team)
+def add_team_lead_as_team_member(sender, instance, update_fields, **kwargs):
+    if not update_fields or 'team_lead' in update_fields:
+        team_member = TeamMember.create_factory(commit=False, team=instance, member=instance.team_lead)
+        team_member.save()
+        return team_member
 
 
 
