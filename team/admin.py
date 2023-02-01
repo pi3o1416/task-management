9d4f314a2df7aa30a0528201551eda568fd8be9c
= <mmonir.m.29@gmail.com>
2023-02-01 16:43:56 +0600

    Add TeamMemberAdmin class to add TeamMember model to admin panel.

--- team/admin.py
+++ team/admin.py
@@ -1,8 +1,13 @@
 
 from django.contrib import admin
-from .models import Team
+from .models import Team, TeamMember
 
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'description', 'team_lead', 'department']
+
+
+@admin.register(TeamMember)
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'team', 'member', 'member_full_name', 'member_username']
