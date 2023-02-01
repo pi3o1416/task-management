9b1dc38651de4cad8790aefb5b7ad79b12841df6
= <mmonir.m.29@gmail.com>
2023-02-01 18:14:56 +0600

    Add TeamMemberViewSet, BulkAddTeamMember, GetMembersOfTeam in urls.

--- team/urls.py
+++ team/urls.py
@@ -2,15 +2,22 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TeamViewSet
+from .views import TeamViewSet, TeamMemberViewSet, GetMembersOfTeam, BulkAddTeamMember
 
 
 team_router = DefaultRouter()
 team_router.register('', TeamViewSet, 'team')
 
+team_member_router = DefaultRouter()
+team_member_router.register('', TeamMemberViewSet, 'team-member')
+
+
 app_name='team'
 urlpatterns = [
-    path('', include(team_router.urls))
+    path('members/', include(team_member_router.urls)),
+    path('<int:team_pk>/members/', GetMembersOfTeam.as_view(), name='members-of-team'),
+    path('<int:team_pk>/bulk-add-members/', BulkAddTeamMember.as_view(), name='bulk-add-member'),
+    path('', include(team_router.urls)),
 ]
 
 
