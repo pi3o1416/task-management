0402c223e22cee38a9bbb01bd6aef0e268a49742
= <mmonir.m.29@gmail.com>
2023-02-01 12:20:49 +0600

    Update fill_team_lead_info signal due to team_lead field foreignkey reference change. Change signal from post_save to pre_save to lower database I/O.

--- team/signals.py
+++ team/signals.py
@@ -1,15 +1,16 @@
 
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import pre_save
 from .models import Team
 
 
-@receiver(signal=post_save, sender=Team)
-def fill_team_lead_info(sender, instance, created, **kwargs):
-    if created:
+@receiver(signal=pre_save, sender=Team)
+def fill_team_lead_info(sender, instance, update_fields, **kwargs):
+    if not update_fields:
         team_lead = instance.team_lead
-        instance.team_lead_full_name = team_lead.member_full_name
-        instance.save()
+        instance.team_lead_full_name = team_lead.full_name
+        instance.team_lead_username = team_lead.username
+    return  instance
 
 
 @receiver(signal=pre_save, sender=Team)
