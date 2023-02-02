94d651486f3d2175c3d74841d0d3d2a6e38d56e6
= <mmonir.m.29@gmail.com>
2023-02-02 13:40:02 +0600

    Create Team task to assign task to team.

---
+++ team/models/team_task_models.py
@@ -0,0 +1,41 @@
+
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+from services.exceptions import InvalidRequest
+
+from task.models import Task
+from .team_models import Team
+
+
+class TeamTasks(models.Model):
+    team = models.ForeignKey(
+        to=Team,
+        on_delete=models.CASCADE,
+        related_name='team_tasks',
+        verbose_name=_("Team"),
+    )
+    task = models.ForeignKey(
+        to=Task,
+        on_delete=models.CASCADE,
+        related_name='task_team',
+        verbose_name=_("Task")
+    )
+
+    @classmethod
+    def create_factory(cls, commit=False, **kwargs):
+        try:
+            assert kwargs.get('task') != None, "Task should not be empty"
+            assert kwargs.get("team") != None, "Team should not be empty"
+            team_task = cls(**kwargs)
+            if commit == True:
+                team_task.save()
+            return team_task
+        except AssertionError as exception:
+            raise InvalidRequest(detail={"detail": _(exception.__str__())})
+        except Exception as exception:
+            raise InvalidRequest(detail={"detail": _(exception.__str__())})
+
+
+
+
+
