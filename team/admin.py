b412b605be067e953127080a1af986133cf918f9
= <mmonir.m.29@gmail.com>
2023-02-02 16:25:26 +0600

    Add TeamTasksAdmin class for admin panel.

--- team/admin.py
+++ team/admin.py
@@ -1,6 +1,6 @@
 
 from django.contrib import admin
-from .models import Team, TeamMember
+from .models import Team, TeamMember, TeamTasks
 
 
 @admin.register(Team)
@@ -11,3 +11,9 @@ class TeamAdmin(admin.ModelAdmin):
 @admin.register(TeamMember)
 class TeamMemberAdmin(admin.ModelAdmin):
     list_display = ['pk', 'team', 'member', 'member_full_name', 'member_username']
+
+
+@admin.register(TeamTasks)
+class TeamTasksAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'team', 'task']
+
