fb4cf255e99b46c57f4f4b95c70c999df1080a65
= <mmonir.m.29@gmail.com>
2023-02-02 15:37:44 +0600

    Update TeamQuerySet. Inherite from Template queryset to apply default queryset behiviors.

--- team/querysets.py
+++ team/querysets.py
@@ -1,36 +1,12 @@
 
-from functools import reduce
-from operator import __and__
-from django.db.models import QuerySet
 from django.db.models.signals import pre_save
 from django.utils.translation import gettext_lazy as _
-from rest_framework.exceptions import NotFound, APIException
-from rest_framework.request import Request
 
-from services.query_services import generate_q_objects_from_query_params, TemplateQuerySet
+from services.query_services import TemplateQuerySet
 
 
-
-class TeamQuerySet(QuerySet):
-    def get_team_by_pk(self, pk):
-        try:
-            team = self.get(pk=pk)
-            return team
-        except self.model.DoesNotExist:
-            raise NotFound(detail={"detail":_("Team with pk={} does not exist".format(pk))})
-        except ValueError:
-            raise NotFound(detail={"detail":_("Team primary key should be an integer")})
-        except Exception as exception:
-            raise NotFound(detail={"detail":_(exception.__str__())})
-
-    def filter_from_query_params(self, request: Request):
-       try:
-           q_objects = generate_q_objects_from_query_params(self.model, request)
-           if q_objects:
-               return self.filter(reduce(__and__, q_objects))
-           return self.all()
-       except Exception as exception:
-           raise APIException(detail={"detail": exception.args})
+class TeamQuerySet(TemplateQuerySet):
+    pass
 
 
 class TeamMemberQuerySet(TemplateQuerySet):
