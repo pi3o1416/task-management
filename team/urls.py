f07cc1e38a00a8a07888a014fac5eb782aefa4ee
= <mmonir.m.29@gmail.com>
2023-02-02 18:01:46 +0600

    Add TeamTaskCreateAndAssign and TeamTaskDetail View to url.

--- team/urls.py
+++ team/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import TeamViewSet, TeamMemberViewSet, GetMembersOfTeam, BulkAddTeamMember
+from .views import TeamViewSet, TeamMemberViewSet, GetMembersOfTeam, BulkAddTeamMember, TeamTaskCreateAndAssign, TeamTasksDetail
 
 
 team_router = DefaultRouter()
@@ -17,6 +17,8 @@ urlpatterns = [
     path('members/', include(team_member_router.urls)),
     path('<int:team_pk>/members/', GetMembersOfTeam.as_view(), name='members-of-team'),
     path('<int:team_pk>/bulk-add-members/', BulkAddTeamMember.as_view(), name='bulk-add-member'),
+    path('tasks/<int:team_task_pk>/', TeamTasksDetail.as_view(), name='team-task-detail'),
+    path('<int:team_pk>/create-assign-task/', TeamTaskCreateAndAssign.as_view(), name='team-task-create-assign'),
     path('', include(team_router.urls)),
 ]
 
