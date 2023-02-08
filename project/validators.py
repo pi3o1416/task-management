001797ca39af3b5c7ff278c37bd5e3ded58f7c22
= <mmonir.m.29@gmail.com>
2023-02-08 16:24:17 +0600

    Add validator for Project model. validator to validate project deadline, budget, project manager permission, project owner permission.

---
+++ project/validators.py
@@ -0,0 +1,41 @@
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ValidationError
+from django.utils import timezone
+
+User = get_user_model()
+
+
+def validate_project_deadline(deadline):
+    """
+    Validate if deadline is greater than today
+    """
+    today = timezone.now().date()
+    if deadline <= today:
+        raise ValidationError(message="Project deadline should be greater than current date")
+    return deadline
+
+
+def validate_budget(budget):
+    """
+    Validate if budget is less than 0
+    """
+    if budget < 0:
+        raise ValidationError(message="Budget should not be less than 0")
+    return budget
+
+
+def validate_project_manager_permission(project_manager):
+    if project_manager.has_perm('can_maintain_project') == True:
+        return project_manager
+    raise ValidationError(message="Project manager does not have permission to maintain project")
+
+
+def validate_project_owner_permission(project_owner):
+    if project_owner.has_perm('project.can_own_project') == True:
+        return project_owner
+    raise ValidationError(message="Project owner does not have permission to own project")
+
+
+
+
