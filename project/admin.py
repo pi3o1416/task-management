35a7209c6c03b6c9c45c7183e78907fb80a170cc
= <mmonir.m.29@gmail.com>
2023-02-08 14:01:15 +0600

    Add Project and ProjectSchemaLessData to admin panel.

--- project/admin.py
+++ project/admin.py
@@ -1,3 +1,26 @@
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project, ProjectSchemaLessData
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'title', 'description', 'deadline', 'budget', 'project_manager',
+                    'project_owner', 'department', 'status']
+
+
+@admin.register(ProjectSchemaLessData)
+class ProjectSchemaLessDataAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'project', 'department_title', 'project_owner_fullname',
+                    'project_owner_username', 'project_manager_fullname',
+                    'project_manager_username']
+
+
+
+
+
+
+
+
+
