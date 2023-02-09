ef617b0dd6fe617fc462673360be927205dc35c2
= <mmonir.m.29@gmail.com>
2023-02-09 14:24:08 +0600

    Add update_project_schemaless_data signal function to update ProjectSchemaLessData model instance on Project update.

--- project/signals.py
+++ project/signals.py
@@ -25,6 +25,24 @@ def create_project_schemaless_data(sender, instance:Project, created=None, **kwa
             raise DBOperationFailed(detail={"detail":_("Project optional data update filed")})
 
 
+@receiver(signal=post_save, sender=Project)
+def update_project_schemaless_data(sender, instance:Project, created=None, update_fields=None, **kwargs):
+    if update_fields:
+        schemaless_data = ProjectSchemaLessData.objects.get(project=instance)
+        if 'department' in update_fields:
+            schemaless_data.department_title = instance.department.name
+        if 'project_manager' in update_fields:
+            schemaless_data.project_manager_username = instance.project_manager.username
+            schemaless_data.project_manager_fullname = instance.project_manager.full_name
+        if 'project_owner' in update_fields:
+            schemaless_data.project_owner_username = instance.project_owner.username
+            schemaless_data.project_onwer_fullname = instance.project_owner.full_name
+        schemaless_data.save()
+
+
+
+
+
 
 
 
