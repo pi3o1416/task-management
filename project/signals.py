0486e065c8bd32c9b79fd980633c2be1009c069b
= <mmonir.m.29@gmail.com>
2023-02-09 18:11:31 +0600

    Add add_project_manager_as_project_member, create_project_member_schemaless_data signal function to add project manager to project member and add project member shcemaless data.

--- project/signals.py
+++ project/signals.py
@@ -2,9 +2,10 @@
 from django.utils.translation import gettext_lazy as _
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+from project.models.project_member_models import ProjectMemberSchemaLessData
 
 from services.exceptions import DBOperationFailed, InvalidRequest
-from .models import Project, ProjectSchemaLessData
+from .models import Project, ProjectSchemaLessData, ProjectMember
 
 
 @receiver(signal=post_save, sender=Project)
@@ -22,7 +23,7 @@ def create_project_schemaless_data(sender, instance:Project, created=None, **kwa
             )
         except InvalidRequest:
             instance.delete()
-            raise DBOperationFailed(detail={"detail":_("Project optional data update filed")})
+            raise DBOperationFailed(detail={"detail":_("Project create cancled due to Project optional data update filed")})
 
 
 @receiver(signal=post_save, sender=Project)
@@ -38,6 +39,41 @@ def update_project_schemaless_data(sender, instance:Project, created=None, updat
             schemaless_data.project_owner_username = instance.project_owner.username
             schemaless_data.project_onwer_fullname = instance.project_owner.full_name
         schemaless_data.save()
+
+
+@receiver(signal=post_save, sender=Project)
+def add_project_manager_as_project_member(sender, instance:Project, created=None, update_fields=None, **kwargs):
+    if created:
+        try:
+            project_manager = instance.project_manager
+            project_member = ProjectMember.create_factory(
+                commit=True,
+                project=instance,
+                member=project_manager
+            )
+            return project_member
+        except DBOperationFailed:
+            return True
+        except InvalidRequest:
+            instance.delete()
+            raise DBOperationFailed(detail=_("Project create cancled due to failure in add project manager as project member"))
+
+
+@receiver(signal=post_save, sender=ProjectMember)
+def create_project_member_schemaless_data(sender, instance:ProjectMember, created=None, update_fields=None, **kwargs):
+    if created:
+        try:
+            project_member_schemaless_data = ProjectMemberSchemaLessData.create_factory(
+                commit=True,
+                project=instance.project,
+                member_username=instance.member.username,
+                member_fullname=instance.member.full_name,
+            )
+            return project_member_schemaless_data
+        except InvalidRequest:
+            instance.delete()
+            raise InvalidRequest(detail={"detail": _("Project Member create cancled due to failure in project schemaless data creation")})
+
 
 
 
