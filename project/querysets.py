4da5de9d9169b9ec6abcd64b92d28dc4df8e98c3
= <mmonir.m.29@gmail.com>
2023-02-09 14:19:38 +0600

    Add queryset method filter_with_reverse_related_fields that filter on reverse related field of Project models.

--- project/querysets.py
+++ project/querysets.py
@@ -1,6 +1,15 @@
 
-from services.querysets import TemplateQuerySet
+from services.querysets import TemplateQuerySet, get_model_foreignkey_fields
 
 
 class ProjectQuerySet(TemplateQuerySet):
-    pass
+    def filter_with_reverse_related_fields(self, request):
+        filtered_projects = self.select_related('schemaless_data').filter_from_query_params(request=request)
+        for related_object in self.model._meta.related_objects:
+            FieldModel = related_object.field.model
+            field_name = related_object.name
+            filtered_projects = filtered_projects.filter_from_query_params(request=request, FieldModel=FieldModel, related_field=field_name)
+        return filtered_projects
+
+
+
