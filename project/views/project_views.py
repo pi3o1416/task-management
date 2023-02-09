dd8c6b06e4131da447c934d36c07a5ac1086a4df
= <mmonir.m.29@gmail.com>
2023-02-09 14:25:08 +0600

    Add CRUD operation for Project model.

--- project/views/project_views.py
+++ project/views/project_views.py
@@ -1,4 +1,6 @@
 
+from django.utils.translation import gettext_lazy as _
+from rest_framework.decorators import action
 from rest_framework.viewsets import ViewSet
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
@@ -6,6 +8,7 @@ from rest_framework import status
 
 
 from ..serializers import ProjectSerializer, ProjectDetailSerializer
+from ..models import Project
 
 
 class ProjectViewSet(ViewSet, PageNumberPagination):
@@ -18,16 +21,43 @@ class ProjectViewSet(ViewSet, PageNumberPagination):
         return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request):
-        pass
+        projects = self.get_queryset(request)
+        page = self.paginate_queryset(queryset=projects, request=request)
+        serializer = self.get_serializer_class()(instance=page, many=True)
+        return self.get_paginated_response(data=serializer.data)
 
-    def retrieve(self, request, project_pk):
-        pass
+    def retrieve(self, request, pk):
+        project = self.get_object(pk=pk)
+        serializer = self.get_serializer_class()(instance=project)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
-    def destroy(self, request, project_pk):
-        pass
+    def destroy(self, request, pk):
+        #TODO: Fix Broken pipe from 127.0.0.1 51792 on 204 response
+        project = self.get_object(pk=pk)
+        project.delete()
+        return Response(data={"detail": _("Project delete successful")}, status=status.HTTP_202_ACCEPTED)
 
-    def update(self, request, project_pk):
-        pass
+    def update(self, request, pk):
+        project = self.get_object(pk=pk)
+        serializer = self.get_serializer_class()(instance=project, data=request.data)
+        if serializer.is_valid():
+            serializer.update()
+            response_serializer = ProjectDetailSerializer(instance=project)
+            return Response(data=response_serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
-        return ProjectSerializer
+        if self.action in ['create', 'update']:
+            return ProjectSerializer
+        return ProjectDetailSerializer
+
+    def get_queryset(self, request):
+        return Project.objects.filter_with_reverse_related_fields(request)
+
+    def get_object(self, pk):
+        return Project.objects.get_object_by_pk(pk=pk)
+
+
+
+
+
