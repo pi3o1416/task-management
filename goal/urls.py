c6320857c92b3c3ab7097c9e77afe34c8b83692f
= <mmonir.m.29@gmail.com>
2023-02-12 16:17:02 +0600

    Add GoalCreateView to goal app urlpatterns.

--- goal/urls.py
+++ goal/urls.py
@@ -2,13 +2,14 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import GoalViewSet, DepartmentGoals
+from .views import GoalViewSet, DepartmentGoals, GoalCreateView
 
 goal_router = DefaultRouter()
 goal_router.register('', GoalViewSet, 'goal')
 
 app_name='goal'
 urlpatterns = [
-    path('', include(goal_router.urls)),
     path('department-goals/<int:department_pk>/', DepartmentGoals.as_view(), name="department_goals"),
+    path('create/<int:department_pk>/', GoalCreateView.as_view(), name='goal-create-view'),
+    path('', include(goal_router.urls)),
 ]
