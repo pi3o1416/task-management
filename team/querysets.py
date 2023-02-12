065abdf113fec01a97e7a888e6b18431eeb57576
= <mmonir.m.29@gmail.com>
2023-02-12 14:07:42 +0600

    :bug: Fix unintentional breakpoint on bulk_create_team_member queryset method.

--- team/querysets.py
+++ team/querysets.py
@@ -13,13 +13,13 @@ class TeamMemberQuerySet(TemplateQuerySet):
     def bulk_create_team_member(self, team_members):
         Model = self.model
         team_members = [pre_save.send(Model, instance=team_member, update_fields=None)[0][1] for team_member in team_members]
-        breakpoint()
         team_members = Model.objects.bulk_create(team_members)
         return team_members
 
 
 class TeamTasksQuerySet(TemplateQuerySet):
     def filter_with_related_fields(self, request):
+        #TODO: Update name to filter with foreignkey field
         #Default filter
         filtered_tasks = self.filter_from_query_params(request=request)
         #Filter for foreignkey relation
