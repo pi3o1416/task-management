2e347949a5ae315e895264d4bde703993c5933ba
= <mmonir.m.29@gmail.com>
2023-02-13 16:47:13 +0600

    Add ProjectAttachmentSerializer to serialize project attachment instance.

--- project/serializers/project_serializers.py
+++ project/serializers/project_serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 
-from ..models import Project, ProjectSchemaLessData
+from ..models import Project, ProjectSchemaLessData, ProjectAttachment
 
 
 class ProjectSerializer(serializers.ModelSerializer):
@@ -14,7 +14,7 @@ class ProjectSerializer(serializers.ModelSerializer):
     def update(self):
         assert self.instance != None, "Initialize serializer with a project instance before update"
         assert self.validated_data != None, "Validate serializer data before update"
-        self.instance = Project.update(instance_pk=self.instance.pk, **self.validated_data)
+        self.instance.update(commit=True, **self.validated_data)
         return self.instance
 
 
@@ -36,6 +36,24 @@ class ProjectDetailSerializer(serializers.ModelSerializer):
                   'project_owner', 'department', 'status', 'schemaless_data']
         read_only_fields = ['pk', 'title', 'description', 'deadline', 'budget', 'project_manager',
                   'project_owner', 'department', 'status', 'schemaless_data']
+
+
+class ProjectAttachmentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ProjectAttachment
+        fields = ['pk', 'project', 'attached_by', 'attachment', 'attached_at',
+                  'attached_by_user_username', 'attached_by_user_fullname']
+        read_only_fields = ['pk', 'project', 'attached_by', 'attached_at',
+                            'attached_by_user_username', 'attached_by_user_fullname']
+
+    def create(self, commit=True):
+        assert self.validated_data != None, "Validate serializer before create instance"
+        project_attachment = ProjectAttachment.create_factory(
+            commit=commit,
+            kwargs=self.validated_data
+        )
+        return project_attachment
+
 
 
 
