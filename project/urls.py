3df41ef4f3fb4fef5169d9e8c4b978b565e1b24c
= <mmonir.m.29@gmail.com>
2023-02-15 17:11:14 +0600

    Add Project member create, list, delete view to urlpatterns.

--- project/urls.py
+++ project/urls.py
@@ -2,7 +2,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectViewSet, DepartmentProjects, ProjectAttachmentCreate, ProjectAttachmentDelete, ProjectAttachmentsList
+from .views import ProjectViewSet, DepartmentProjects, ProjectAttachmentCreate, ProjectAttachmentDelete, ProjectAttachmentsList, ProjectMemberCreateView, MembersOfProjectView, ProjectMemberDeleteView
 
 
 project_router = DefaultRouter()
@@ -15,6 +15,9 @@ urlpatterns = [
     path('<int:project_pk>/create-attachment/', ProjectAttachmentCreate.as_view(), name='create_attachment'),
     path('<int:project_pk>/attachments/', ProjectAttachmentsList.as_view(), name='project_attachments_list'),
     path('attachments/delete/<int:attachment_pk>/', ProjectAttachmentDelete.as_view(), name='delete_attachment'),
+    path('<int:project_pk>/members-add/', ProjectMemberCreateView.as_view(), name='project_member_add'),
+    path('<int:project_pk>/members/', MembersOfProjectView.as_view(), name='members_of_project'),
+    path('<int:project_pk>/members/<int:user_pk>/delete/', ProjectMemberDeleteView.as_view(), name='delete_project_member'),
     path('', include(project_router.urls)),
 ]
 
