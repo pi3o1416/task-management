8dd456daf98760e2030345857f48de5d9ff1f991
= <mmonir.m.29@gmail.com>
2023-02-15 17:08:03 +0600

    Remove ProjectMemberAdmin, add get_member method to ProjectAdmin.

--- project/admin.py
+++ project/admin.py
@@ -1,13 +1,19 @@
 
 from django.contrib import admin
 
-from .models import Project, ProjectSchemaLessData, ProjectAttachment, ProjectMember, ProjectTask
+from .models import Project, ProjectSchemaLessData, ProjectAttachment, ProjectTask
 
 
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'description', 'deadline', 'budget', 'project_manager',
-                    'project_owner', 'department', 'status']
+                    'project_owner', 'department', 'status', 'get_members']
+
+    def get_members(self, project:Project):
+        project_members = project.members.all()
+        if project_members:
+            return [member.username for member in project_members]
+        return project.title
 
 
 @admin.register(ProjectSchemaLessData)
@@ -15,6 +21,7 @@ class ProjectSchemaLessDataAdmin(admin.ModelAdmin):
     list_display = ['pk', 'project', 'department_title', 'project_owner_fullname',
                     'project_owner_username', 'project_manager_fullname',
                     'project_manager_username']
+
 
 @admin.register(ProjectAttachment)
 class ProjectAttachmentAdmin(admin.ModelAdmin):
@@ -26,10 +33,6 @@ class ProjectAttachmentAdmin(admin.ModelAdmin):
 class ProjectTaskAdmin(admin.ModelAdmin):
     list_display = ['pk', 'project', 'task']
 
-
-@admin.register(ProjectMember)
-class ProjectMemberAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'project', 'member', 'member_username', 'member_fullname']
 
 
 
