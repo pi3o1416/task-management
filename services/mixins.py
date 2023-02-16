cdf0c268c294aeb9c9ab73ff9fe35059d52bea0d
= <mmonir.m.29@gmail.com>
2023-02-16 13:48:12 +0600

    Update ModelUpdateMixin. Organize logic to update model field.

--- services/mixins.py
+++ services/mixins.py
@@ -9,23 +9,26 @@ class ModelUpdateMixins(models.Model):
         abstract=True
 
     def update(self, commit=True, **kwargs):
-        assert self.error_messages, "Create error_messages field on your model"
-        assert self.restricted_fields, "Add restricted_fields field on your model"
-        previous_state = self
+        assert hasattr(self, 'error_messages'), "Create error_messages field on your model"
+        assert hasattr(self, 'restricted_fields'), "Add restricted_fields field on your model"
+        assert type(self.error_messages) is type(dict()), "error_messages field should be an dict"
+        assert "UPDATE" in self.error_messages, "UPDATE key is absent from error_messages dictionary"
         fields = [field.name for field in self._meta.fields]
         for key, value in kwargs.items():
-            if key in fields:
-                setattr(self, key, value)
-            else:
-                self = previous_state
+            if key not in fields:
                 raise InvalidRequest(detail={
-                    "detail": _("{} {}".format(self.error_messages["UPDATE"]))
+                    "detail": _("{} {} is an invalid field".format(self.error_messages["UPDATE"], key))
                 })
+            if key in self.restricted_fields:
+                raise InvalidRequest(detail={
+                    "detail": _("{} {} is prohabited from any update".format(self.error_messages["UPDATE"], key))
+                })
+            setattr(self, key, value)
         if commit == True:
-            if self.pk != None:
-                self.save(update_fields=kwargs.keys())
-            else:
+            if self.pk == None:
                 self.save()
+            else:
+                self.save(update_fields=kwargs.keys())
         return True
 
 
