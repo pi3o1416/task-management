e92d965ea35c73d1a93fb6dc11a4bd080842e1aa
= <mmonir.m.29@gmail.com>
2023-02-19 18:10:09 +0600

    Add TemplateAPIView and TemplateViewSet. This template will be used all over the project.

---
+++ services/views.py
@@ -0,0 +1,29 @@
+
+from rest_framework.viewsets import ViewSet
+from rest_framework.views import APIView
+
+
+class TemplateAPIView(APIView):
+    def __init__(self, model=None):
+        self.model = model
+
+    def get_object(self, pk):
+        assert self.model != None, "Initialize model before get object"
+        obj = self.model.objects.get_object_by_pk(pk=pk)
+        self.check_object_permissions(self.request, obj=obj)
+
+
+class TemplateViewSet(ViewSet):
+    def __init__(self, model=None):
+        self.model = model
+
+    def get_object(self, pk):
+        assert self.model != None, "Initialize model before get object"
+        obj = self.model.objects.get_object_by_pk(pk=pk)
+        self.check_object_permissions(self.request, obj=obj)
+
+
+
+
+
+
