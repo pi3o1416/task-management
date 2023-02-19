ebd66b79f73d2e0cfeeb1ede8dc3f2e22cea21af
= <mmonir.m.29@gmail.com>
2023-02-19 13:10:32 +0600

    Add documentation for account active, deactive by admin, staff permission grant, deny, get_authenticated_user, upload photo.

--- authentication/documentations/basic_view_docs.py
+++ authentication/documentations/basic_view_docs.py
@@ -108,6 +108,120 @@ UserUpdateDoc = Documentation(
     }
 )
 
+GetAuthenticatedUserDoc = Documentation(
+    request=None,
+    responses={
+        200: OpenApiResponse(
+            response=ResponseSerializer,
+            examples=[
+                OpenApiExample(
+                    name="Get authenticated user detail",
+                    value={
+                        "response_data": UserSerializer(UserFactory.build()).data,
+                        "status": 200,
+                        "error": None,
+                    },
+                    response_only=True,
+                    status_codes=[200]
+                )
+            ]
+        )
+    }
+)
+
+ActiveAccountByAdminDoc = Documentation(
+    request=None,
+    responses={
+        202: OpenApiResponse(
+            response=ResponseSerializer,
+            examples=[
+                OpenApiExample(
+                    name="Active user account by admin",
+                    value={
+                        "response_data": {
+                            "detail": "success_message"
+                        },
+                        "status": 202,
+                        "error": None,
+                    },
+                    response_only=True,
+                    status_codes=[202]
+                )
+            ]
+        )
+    }
+)
+
+DeactiveAccountByAdminDoc = Documentation(
+    request=None,
+    responses={
+        202: OpenApiResponse(
+            response=ResponseSerializer,
+            examples=[
+                OpenApiExample(
+                    name="Deactive user account by admin",
+                    value={
+                        "response_data": {
+                            "detail": "success_message"
+                        },
+                        "status": 202,
+                        "error": None,
+                    },
+                    response_only=True,
+                    status_codes=[202]
+                )
+            ]
+        )
+    }
+)
+
+GrantStaffPermissionDoc = Documentation(
+    request=None,
+    responses={
+        202: OpenApiResponse(
+            response=ResponseSerializer,
+            examples=[
+                OpenApiExample(
+                    name="Grant user staff permission",
+                    value={
+                        "response_data": {
+                            "detail": "success_message"
+                        },
+                        "status": 202,
+                        "error": None,
+                    },
+                    response_only=True,
+                    status_codes=[202]
+                )
+            ]
+        )
+    }
+)
+
+
+RemoveStaffPermissionDoc = Documentation(
+    request=None,
+    responses={
+        202: OpenApiResponse(
+            response=ResponseSerializer,
+            examples=[
+                OpenApiExample(
+                    name="Remove user staff permission",
+                    value={
+                        "response_data": {
+                            "detail": "success_message"
+                        },
+                        "status": 202,
+                        "error": None,
+                    },
+                    response_only=True,
+                    status_codes=[202]
+                )
+            ]
+        )
+    }
+)
+
 ActiveAccountDoc = Documentation(
     responses={
         202: OpenApiResponse(
@@ -121,6 +235,24 @@ ActiveAccountDoc = Documentation(
                         },
                         "status":202,
                         "error":None,
+                    },
+                    response_only=True,
+                    status_codes=[202]
+                )
+            ]
+        ),
+    }
+)
+
+UploadUserPhotoDoc = Documentation(
+    responses={
+        202: OpenApiResponse(
+            response=ResponseSerializer,
+            examples=[
+                OpenApiExample(
+                    name="User Account Activation Successful",
+                    value={
+                        "photo": "some url"
                     },
                     response_only=True,
                     status_codes=[202]
