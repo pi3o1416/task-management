24082a3d96616215d06387fa08af3756c2f292d2
= <mmonir.m.29@gmail.com>
2023-02-22 18:07:21 +0600

    Extend Designation views and viewsets from TemplateAPIView and TemplateViewSet.

--- department/views/designation_views.py
+++ department/views/designation_views.py
@@ -1,17 +1,21 @@
 
-from rest_framework.viewsets import ViewSet
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from drf_spectacular.utils import extend_schema
-from ..exceptions import DesignationGetException
-from ..pagination import CustomPageNumberPagination
-from ..serializers import DesignationSerializer
-from ..models import Designations
+from rest_framework import status
+from rest_framework.permissions import DjangoModelPermissions, IsAuthenticated
+from rest_framework.response import Response
+
+from services.pagination import CustomPageNumberPagination
+from services.views import TemplateViewSet, TemplateAPIView
+
 from ..documentations import designation_docs as docs
+from ..models import Designations
+from ..serializers import DesignationSerializer
 
 
-class DesignationViewSet(ViewSet, CustomPageNumberPagination):
+class DesignationViewSet(TemplateViewSet, CustomPageNumberPagination):
+    model = Designations
+    queryset = Designations.objects.all()
+    permission_classes = [DjangoModelPermissions]
 
     @extend_schema(
         responses=docs.DesignationViewSetCreateDoc.responses,
@@ -37,8 +41,7 @@ class DesignationViewSet(ViewSet, CustomPageNumberPagination):
         List of All Designation.
         """
         serializer_class = self.get_serializer_class()
-        designations = Designations.objects.all()
-        page = self.paginate_queryset(queryset=designations, request=request)
+        page = self.paginate_queryset(queryset=self.queryset, request=request)
         serializer = serializer_class(instance=page, many=True)
         return self.get_paginated_response(serializer.data)
 
@@ -51,12 +54,9 @@ class DesignationViewSet(ViewSet, CustomPageNumberPagination):
         Destroy Department
         URL Parameter: Designation Primary Key(int)
         """
-        try:
-            designation = Designations.objects.get_designation(pk=pk)
-            designation.delete()
-            return Response(data={"detail": ("Designation delete successful",)}, status=status.HTTP_200_OK)
-        except DesignationGetException as exception:
-            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+        designation = self.get_object(pk=pk)
+        designation.delete()
+        return Response(data={"detail": ("Designation delete successful",)}, status=status.HTTP_200_OK)
 
     @extend_schema(
         responses=docs.DesignationViewSetRetrieveDoc.responses,
@@ -67,13 +67,10 @@ class DesignationViewSet(ViewSet, CustomPageNumberPagination):
         Retrieve Designation Object
         URL Parameter: Designation Primary Key(int)
         """
-        try:
-            designation = Designations.objects.get_designation(pk=pk)
-            serializer_class = self.get_serializer_class()
-            serializer = serializer_class(instance=designation)
-            return Response(data=serializer.data, status=status.HTTP_200_OK)
-        except DesignationGetException as exception:
-            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+        designation = self.get_object(pk=pk)
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(instance=designation)
+        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     @extend_schema(
         responses=docs.DesignationViewSetUpdateDoc.responses,
@@ -84,43 +81,34 @@ class DesignationViewSet(ViewSet, CustomPageNumberPagination):
         Update Designation Object
         URL Parameter: Designation Primary Key(int)
         """
-        try:
-            designation = Designations.objects.get_designation(pk=pk)
-            serializer_class = self.get_serializer_class()
-            serializer = serializer_class(instance=designation, data=request.data)
-            if serializer.is_valid():
-                serializer.update()
-                return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
-            return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-        except DesignationGetException as exception:
-            return Response(data={"detail": exception.args}, status=status.HTTP_404_NOT_FOUND)
+        designation = self.get_object(pk=pk)
+        serializer_class = self.get_serializer_class()
+        serializer = serializer_class(instance=designation, data=request.data)
+        if serializer.is_valid():
+            serializer.update()
+            return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def get_serializer_class(self):
         return DesignationSerializer
 
 
-class DepartmentDesignationsView(APIView, CustomPageNumberPagination):
+class DepartmentDesignationsView(TemplateAPIView, CustomPageNumberPagination):
+    permission_classes = [IsAuthenticated]
+    queryset = Designations.objects.all()
+    model = Designations
 
     @extend_schema(
         responses=docs.DepartmentDesignationsDoc.responses,
         parameters=docs.DepartmentDesignationsDoc.parameters
     )
     def get(self, request, department_pk):
-        designations = Designations.objects.get_department_designations(department_pk=department_pk)
+        """
+        Designations of a department
+        """
+        designations = self.queryset.get_department_designations(department_pk=department_pk)
         page = self.paginate_queryset(queryset=designations, request=request)
         serializer = DesignationSerializer(instance=page, many=True)
         return self.get_paginated_response(serializer.data)
-
-
-
-
-
-
-
-
-
-
-
-
 
 
