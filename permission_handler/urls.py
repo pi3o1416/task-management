91d8f5ecaea2ce561b30a6b1f75d056fe4d6a5c4
= <mmonir.m.29@gmail.com>
2023-02-22 12:49:13 +0600

    Add RemoveFromGroup APIView to urlpatterns.

--- permission_handler/urls.py
+++ permission_handler/urls.py
@@ -1,7 +1,7 @@
 
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import PermissionViewSet, GroupViewSet, AssignGroup, AllUserPermissions, UserGroups
+from .views import PermissionViewSet, GroupViewSet, AssignGroup, AllUserPermissions, UserGroups, RemoveFromGroup
 
 
 permission_router = DefaultRouter()
@@ -13,10 +13,11 @@ group_router.register('', GroupViewSet, 'group')
 app_name='permission_handler'
 urlpatterns = [
     path('permissions/', include(permission_router.urls)),
+    path('groups/<int:group_pk>/remove-user/<int:user_pk>/', RemoveFromGroup.as_view(), name='remove_from_group'),
     path('groups/', include(group_router.urls)),
-    path('assign-group/', AssignGroup.as_view(), name='assign-group'),
-    path('user/<int:user_pk>/user-permissions/', AllUserPermissions.as_view(), name='user-permissions'),
-    path('user/<int:user_pk>/user-groups/', UserGroups.as_view(), name='user-groups'),
+    path('assign-group/', AssignGroup.as_view(), name='assign_group'),
+    path('user/<int:user_pk>/user-permissions/', AllUserPermissions.as_view(), name='user_permissions'),
+    path('user/<int:user_pk>/user-groups/', UserGroups.as_view(), name='user_groups'),
 ]
 
 
