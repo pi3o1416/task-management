d4fcc32869d199999659efadb8cefdc871adb42d
= <mmonir.m.29@gmail.com>
2023-02-22 18:05:36 +0600

    Remove member_full_name update. this field is deleted from DepartmentMember model.

--- department/signals.py
+++ department/signals.py
@@ -1,13 +1,14 @@
 
-from django.dispatch import receiver
 from django.db.models.signals import pre_save
+from django.dispatch import receiver
+
 from .models import DepartmentMember
 
 
 @receiver(signal=pre_save, sender=DepartmentMember)
 def fill_department_designation_user_info(sender, instance, **kwargs):
+    breakpoint()
     instance.department_name = instance.department.name
-    instance.member_full_name = instance.member.full_name
     instance.designation_title = instance.designation.title
     return instance
 
