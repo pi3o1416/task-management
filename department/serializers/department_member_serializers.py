83228757b7d5170a2a2e74fee5467f441de0f904
= <mmonir.m.29@gmail.com>
2023-02-22 13:42:14 +0600

    Remove member_full_name field from DepartmentMember Serializer.

--- department/serializers/department_member_serializers.py
+++ department/serializers/department_member_serializers.py
@@ -6,15 +6,31 @@ from ..models import DepartmentMember
 class DepartmentMemberSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepartmentMember
-        fields = ['pk', 'member', 'department', 'designation', 'department_name', 'designation_title', 'member_full_name']
-        read_only_fields = ['pk', 'department_name', 'designation_title', 'member_full_name']
+        fields = ['pk', 'member', 'department', 'designation', 'department_name', 'designation_title']
+        read_only_fields = ['pk', 'department_name', 'designation_title']
 
 
 class DepartmentMemberUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model = DepartmentMember
-        fields = ['pk', 'member', 'department', 'designation', 'department_name', 'designation_title', 'member_full_name']
-        read_only_fields = ['pk', 'member', 'department_name', 'designation_title', 'member_full_name']
+        fields = ['pk', 'member', 'department', 'designation', 'department_name', 'designation_title']
+        read_only_fields = ['pk', 'member', 'department_name', 'designation_title']
+
+
+class UserSerializer(serializers.Serializer):
+    pk = serializers.IntegerField()
+    username = serializers.CharField()
+    first_name = serializers.CharField()
+    last_name = serializers.CharField()
+    email = serializers.CharField()
+    photo = serializers.URLField()
+
+
+class DepartmentMemberDetailSerializer(serializers.ModelSerializer):
+    member = UserSerializer()
+    class Meta:
+        model = DepartmentMember
+        fields = '__all__'
 
 
 class DepartmentMemberPaginatedSerializer(serializers.Serializer):
@@ -25,5 +41,6 @@ class DepartmentMemberPaginatedSerializer(serializers.Serializer):
     next = serializers.URLField()
     previous = serializers.URLField()
     results = DepartmentMemberSerializer(many=True)
+
 
 
