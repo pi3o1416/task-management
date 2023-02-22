d62efa8bc056c40e14179f7ecc2ec547e289ef02
= <mmonir.m.29@gmail.com>
2023-02-22 12:33:30 +0600

    Remove __init__ constructor from APIView and TemplateAPIView.

--- services/views.py
+++ services/views.py
@@ -1,27 +1,22 @@
 
+from django.db.models import Model
 from rest_framework.viewsets import ViewSet
 from rest_framework.views import APIView
 
 
 class TemplateAPIView(APIView):
-    def __init__(self, model=None, *args, **kwargs):
-        self.model = model
-        super().__init__(*args, **kwargs)
-
     def get_object(self, pk):
-        assert self.model != None, "Initialize model before get object"
+        assert hasattr(self, 'model'), "Define a model attribute on your class"
+        assert type(self.model) is type(Model), "model should be inherited from ModelBase"
         obj = self.model.objects.get_object_by_pk(pk=pk)
         self.check_object_permissions(self.request, obj=obj)
         return obj
 
 
 class TemplateViewSet(ViewSet):
-    def __init__(self, model=None, *args, **kwargs):
-        self.model = model
-        super().__init__(*args, **kwargs)
-
     def get_object(self, pk):
-        assert self.model != None, "Initialize model before get object"
+        assert hasattr(self, 'model'), "Define a model attribute on your class"
+        assert type(self.model) is type(Model), "model should be inherited from ModelBase"
         obj = self.model.objects.get_object_by_pk(pk=pk)
         self.check_object_permissions(self.request, obj=obj)
         return obj
