1f4560b05cd1ff8eaed7f0ce3fdbb78066d7c59e
= <mmonir.m.29@gmail.com>
2023-02-23 14:10:51 +0600

    Remove signal to populate user_username and user_fullname field of UsersTasks model.

--- task/signals.py
+++ task/signals.py
@@ -1,16 +1,7 @@
 
 from django.dispatch import receiver
 from django.db.models.signals import post_save
-from .models import UsersTasks, Task, TaskTree
-
-@receiver(signal=post_save, sender=UsersTasks)
-def change_task_type_and_assignment_status(sender, instance, created, **kwargs):
-    if created:
-        task = instance.task
-        task.is_assigned=True
-        task.task_type = Task.TaskType.USER_TASK
-        task.save(update_fields=['is_assigned', 'task_type'])
-
+from .models import TaskTree
 
 @receiver(signal=post_save, sender=TaskTree)
 def update_has_subtask_fields_of_task_instance(sender, instance, created, **kwargs):
