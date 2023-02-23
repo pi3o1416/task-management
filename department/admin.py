86b5c3e570be8cc8d70d03a2a6774479e976226a
= <mmonir.m.29@gmail.com>
2023-02-23 14:32:32 +0600

    Remove hard code field name from Departmen app models admin panel.

--- department/admin.py
+++ department/admin.py
@@ -5,18 +5,17 @@ from .models import Designations, Department, DepartmentMember
 
 @admin.register(Designations)
 class DesignationsAdmin(admin.ModelAdmin):
-    list_display = ["pk", "department", "title"]
-    pass
+    list_display = [field.name for field in Designations._meta.fields]
 
 
 @admin.register(Department)
 class DepartmentAdmin(admin.ModelAdmin):
-    list_display = ["pk", "name", "slug", "description"]
+    list_display = [field.name for field in Department._meta.fields]
 
 
 @admin.register(DepartmentMember)
 class DepartmentMemberAdmin(admin.ModelAdmin):
-    list_display = ["pk", "member", "department", "designation", "is_head"]
+    list_display = [field.name for field in DepartmentMember._meta.fields]
 
 
 
