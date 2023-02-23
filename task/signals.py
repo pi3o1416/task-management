6617f34680deec6530bbb8950e27abd2bd13ac90
= <mmonir.m.29@gmail.com>
2023-02-23 13:41:42 +0600

    Remove signal to populate Task model created_by_user_username and created_by_user_fullname.

--- task/signals.py
+++ task/signals.py
@@ -1,22 +1,7 @@
 
 from django.dispatch import receiver
-from django.db.models.signals import pre_save, post_save
+from django.db.models.signals import post_save
 from .models import UsersTasks, Task, TaskTree
-
-@receiver(signal=pre_save, sender=UsersTasks)
-def fill_users_tasks_user_info(sender, instance, **kwargs):
-    instance.user_username = instance.assigned_to.username
-    instance.user_full_name = instance.assigned_to.full_name
-    return instance
-
-
-@receiver(signal=pre_save, sender=Task)
-def fill_task_created_by_user_info(sender, instance, update_fields=None, **kwargs):
-    if update_fields == None:
-        instance.created_by_user_username = instance.created_by.username
-        instance.created_by_user_fullname = instance.created_by.full_name
-        return instance
-
 
 @receiver(signal=post_save, sender=UsersTasks)
 def change_task_type_and_assignment_status(sender, instance, created, **kwargs):
