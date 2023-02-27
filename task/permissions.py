ac47dd8347f734475aebd7fd63fa5d4a3dd8cc5d
= <mmonir.m.29@gmail.com>
2023-02-27 17:47:44 +0600

    Add permission classes for Task model ViewSet.

--- task/permissions.py
+++ task/permissions.py
@@ -1,39 +1,73 @@
 
-from django.contrib.auth import get_user_model
+from django.forms import model_to_dict
 from rest_framework.permissions import BasePermission
-from .models import Task
 
+from services.decorators import is_authenticated, has_kperms
+from .models import Task, UsersTasks
+
+
+@has_kperms(['task.can_view_all_tasks'])
 class CanViewAllTasks(BasePermission):
-    def has_permission(self, request, view):
-        user = request.user
-        if user.has_perm('task.can_view_all_tasks'):
-            return True
-        return False
+    pass
 
 
-class IsOwner(BasePermission):
+@has_kperms(['task.add_task'])
+class CanCreateTask(BasePermission):
+    pass
+
+
+@has_kperms(['task.delete_task'])
+class CanDeleteTask(BasePermission):
+    pass
+
+
+@has_kperms(['task.change_task'])
+class CanUpdateTask(BasePermission):
+    pass
+
+
+@has_kperms(['task.can_approve_disapprove_task'])
+class HasPermissionToApproveTask(BasePermission):
+    pass
+
+
+class IsTaskOwner(BasePermission):
+    @is_authenticated
     def has_object_permission(self, request, view, task:Task):
         user = request.user
         if task.created_by == user:
             return True
         return False
 
-
-class HasPermissionToApproveTask(BasePermission):
-    def has_permission(self, request, view):
+class IsTaskAssignee(BasePermission):
+    @is_authenticated
+    def has_object_permission(self, request, view, task:Task):
         user = request.user
-        if user.has_perm('task.can_approve_disapprove_task'):
+        if task.task_type == Task.TaskType.USER_TASK:
+            assignee_pk = UsersTasks.objects.values_list('assigned_to', flat=True).get(task=task)
+            if user.pk == assignee_pk:
+                return True
+        return False
+
+
+class IsTaskParents(BasePermission):
+    @is_authenticated
+    def has_object_permission(self, request, view, task:Task):
+        user = request.user
+        parent_tasks_owners = Task.objects.values_list('created_by', flat=True) \
+            .filter(pk__in=task.parent_tasks)
+        if user.pk in parent_tasks_owners:
             return True
         return False
 
 
-class IsAssignedUponUser(BasePermission):
-    def has_object_permission(self, request, view, task):
+class IsAttachmentOwner(BasePermission):
+    @is_authenticated
+    def has_object_permission(self, request, view, attachment):
         user = request.user
-        if task.task_assigned_to.assigned_to == user:
+        if user.pk == model_to_dict(attachment).get('attached_by'):
             return True
         return False
-
 
 
 
