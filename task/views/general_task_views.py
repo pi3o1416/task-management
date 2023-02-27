5782d35c71a21fc36636d7040410e5cb83a05703
= <mmonir.m.29@gmail.com>
2023-02-27 17:54:00 +0600

    Add Permission on TaskViewSet and TaskAttachment Views.

--- task/views/general_task_views.py
+++ task/views/general_task_views.py
@@ -1,34 +1,41 @@
 
 from django.utils.translation import gettext_lazy as _
-from rest_framework.viewsets import ViewSet
-from rest_framework.views import APIView
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.request import Request
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.decorators import action
 
-from ..permissions import CanViewAllTasks, IsOwner, HasPermissionToApproveTask, IsAssignedUponUser
+from services.pagination import CustomPageNumberPagination
+from services.views import TemplateViewSet, TemplateAPIView
+from ..permissions import CanViewAllTasks, IsTaskAssignee, IsTaskOwner, IsTaskParents
+from ..permissions import HasPermissionToApproveTask, CanCreateTask, CanDeleteTask, CanUpdateTask
+from ..permissions import IsAttachmentOwner
 from ..serializers import TaskSerializer, TaskAttachmentsSerializer, TaskStatusStatisticsSerializer
 from ..models import Task, TaskAttachments
 
 
-class TaskViewSet(ViewSet, PageNumberPagination):
+class TaskViewSet(TemplateViewSet, CustomPageNumberPagination):
     #TODO: need reapproval from department head on task update
+    model = Task
+
     def create(self, request):
-        user = request.user
+        """
+        Create new task
+        """
         serializer = self.get_serializer_class()(data=request.data)
         if serializer.is_valid():
-            task = serializer.create(commit=False)
-            task.update_task_owner(user, commit=False)
-            task.save()
-            #Update serializer with populate fields.
-            serializer = self.get_serializer_class()(instance=task)
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+            task = serializer.create(created_by=request.user)
+            response_serializer = TaskSerializer(instance=task)
+            return Response(data=response_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(
+            data={"field_errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     def list(self, request):
+        """
+        List of all tasks
+        """
         tasks = Task.objects.filter_from_query_params(request)
         page = self.paginate_queryset(queryset=tasks, request=request)
         serializer = self.get_serializer_class()(instance=page, many=True)
@@ -36,102 +43,146 @@ class TaskViewSet(ViewSet, PageNumberPagination):
 
     @action(methods=['get'], detail=False, url_path="all-task-statistics")
     def task_list_statistics(self, request):
+        """
+        Task status statistics
+        """
         tasks = Task.objects.filter_from_query_params(request)
         tasks_statistics = tasks.get_task_status_statistics()
         serializer = self.get_serializer_class()(instance=tasks_statistics, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk):
+        """
+        Delete task with maintaining proper restriction
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
+        task.safe_delete()
+        return Response(
+            data={"detail": [_("Task delete successful")]},
+            status=status.HTTP_202_ACCEPTED
+        )
+
+    @action(methods=['delete'], detail=True, url_path='force-delete')
+    def force_destroy(self, request, pk):
+        """
+        Delete task avoiding any restriction
+        """
+        task = self.get_object(pk)
         task.delete()
-        return Response(data={"detail": [_("Task delete successful")]}, status=status.HTTP_202_ACCEPTED)
+        return Response(
+            data={"detail": _("Task delete successful")},
+            status=status.HTTP_202_ACCEPTED
+        )
 
     def retrieve(self, request, pk):
+        """
+        Retrieve Task by primary key
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
         serializer = self.get_serializer_class()(instance=task)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def update(self, request, pk):
+        """
+        Update a task instance
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
         serializer = self.get_serializer_class()(instance=task, data=request.data)
         if serializer.is_valid():
-            serializer.update(instance=task, validated_data=serializer.validated_data)
+            serializer.update(instance=task)
             return Response(data=serializer.data, status=status.HTTP_202_ACCEPTED)
-        return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+        return Response(
+            data={"field_errors": serializer.errors},
+            status=status.HTTP_400_BAD_REQUEST
+        )
 
     @action(methods=['patch'], detail=True, url_path='approve-task')
     def approve_task(self, request, pk):
+        """
+        Update a task approval status to Approved
+        """
         task = self.get_object(pk)
         task.approve_task()
-        return Response(data={"detail": [_("Task with pk={} approved".format(pk))]}, status=status.HTTP_202_ACCEPTED)
+        return Response(
+            data={"detail": [_("Task approval successful".format(pk))]},
+            status=status.HTTP_202_ACCEPTED
+        )
 
     @action(methods=['patch'], detail=True, url_path='disapprove-task')
     def disapprove_task(self, request, pk):
+        """
+        Disapprove a task
+        """
         task = self.get_object(pk)
         task.reject_approval_request()
-        return Response(data={"detail": [_("Task with pk={} has been denied")]})
+        return Response(data={"detail": [_("Task approval denied")]})
 
     @action(methods=['patch'], detail=True, url_path='start-task')
     def start_task(self, request, pk):
+        """
+        Start a task, Status change from pending to due
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
         task.start_task()
         return Response(data={"detail": [_("Task start successful")]})
 
     @action(methods=['patch'], detail=True, url_path='submit-task')
     def submit_task(self, request, pk):
+        """
+        Submit a task, Status change from due to submitted
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
         task.submit_task()
         return Response(data={"detail": [_("Task submission successful")]})
 
     @action(methods=['patch'], detail=True, url_path='accept-submission')
     def accept_task_submission(self, request, pk):
+        """
+        Accept a task submission, Status change from submitted to completed
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
         task.accept_task_submission()
         return Response(data={"detail": [_("Task submission accepted")]})
 
     @action(methods=['patch'], detail=True, url_path='reject-submission')
     def reject_task_submission(self, request, pk):
+        """
+        Reject a task submission, Status change back to due.
+        """
         task = self.get_object(pk)
-        self.check_object_permissions(request, task)
         task.reject_task_submission()
         return Response(data={"detail": [_("Task submission rejected")]})
 
-    def get_object(self, pk):
-        task = Task.objects.get_task_by_pk(pk)
-        return task
+    def get_permissions(self):
+        permissions = []
+        if self.action == 'create':
+            permissions += [CanCreateTask]
+        elif self.action in ['list', 'task_list_statistics']:
+            permissions += [CanViewAllTasks]
+        elif self.action == 'force_delete':
+            permissions += [CanDeleteTask]
+        elif self.action == 'retrieve':
+            permissions += [IsTaskOwner|IsTaskAssignee|IsTaskParents]
+        elif self.action == 'update':
+            permissions += [IsTaskOwner, CanUpdateTask]
+        elif self.action in ['approve_task', 'disapprove_task']:
+            permissions += [HasPermissionToApproveTask]
+        elif self.action in ['start_task', 'submit_task']:
+            permissions += [IsTaskAssignee]
+        elif self.action in ['destroy', 'accept_task_submission', 'reject_task_submission']:
+            permissions += [IsTaskOwner]
+        return [permission() for permission in permissions]
 
     def get_serializer_class(self):
         if self.action == 'task_list_statistics':
             return TaskStatusStatisticsSerializer
         return TaskSerializer
 
-    def get_permissions(self):
-        permission_classes = [IsAuthenticated]
-        if self.action == 'create':
-            permission_classes += []
-        elif self.action == 'list':
-            permission_classes += [CanViewAllTasks]
-        elif self.action == 'retrieve':
-            permission_classes += [IsOwner|CanViewAllTasks|IsAssignedUponUser]
-        elif self.action in ['destroy', 'update']:
-            permission_classes += [IsOwner]
-        elif self.action in ['approve_task', 'disapprove_task']:
-            permission_classes += [HasPermissionToApproveTask]
-        elif self.action in ['start_task', 'submit_task']:
-            permission_classes += [IsAssignedUponUser]
-        elif self.action in ['accept_task_submission', 'reject_task_submission']:
-            permission_classes += [IsOwner]
-        return [permission() for permission in permission_classes]
 
-
-class TaskAttachmentsAdd(APIView):
+class TaskAttachmentsAdd(TemplateAPIView):
+    permission_classes = [IsTaskOwner|IsTaskAssignee]
     serializer_class = TaskAttachmentsSerializer
+    model = Task
 
     def post(self, request:Request, task_pk):
         attachment = request.FILES.get("attachment")
@@ -146,37 +197,30 @@ class TaskAttachmentsAdd(APIView):
         serializer = self.serializer_class(instance=task_attachment)
         return Response(data=serializer.data, status=status.HTTP_201_CREATED)
 
-    def get_object(self, pk):
-        task = Task.objects.get_task_by_pk(pk=pk)
-        return task
 
-
-class TaskAttachmentsDelete(APIView):
+class TaskAttachmentsDelete(TemplateAPIView):
+    permission_classes = [IsAttachmentOwner]
     serializer_class = TaskAttachmentsSerializer
+    model = TaskAttachments
 
     def delete(self, request:Request, task_attachment_pk):
-        task_attachment = TaskAttachments.objects.get_attachment_by_pk(pk=task_attachment_pk)
+        task_attachment = self.get_object(pk=task_attachment_pk)
         task_attachment.delete()
-        return Response(data={"detail": [_("Attachment delete successful")]}, status=status.HTTP_202_ACCEPTED)
+        return Response(
+            data={"detail": [_("Attachment delete successful")]},
+            status=status.HTTP_202_ACCEPTED
+        )
 
 
-class TaskBasedAttachments(APIView):
+class TaskBasedAttachments(TemplateAPIView):
+    permission_classes = [IsTaskOwner|IsTaskAssignee|IsTaskParents]
     serializer_class = TaskAttachmentsSerializer
+    model = Task
 
     def get(self, request:Request, task_pk):
         task = self.get_object(pk=task_pk)
         attachments = task.task_attachments.all()
         serializer = self.serializer_class(instance=attachments, many=True)
         return Response(data=serializer.data, status=status.HTTP_200_OK)
-
-    def get_object(self, pk):
-        task = Task.objects.get_task_by_pk(pk=pk)
-        return task
-
-
-
-
-
-
 
 
