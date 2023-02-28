dcb1e29d99cee7f004a1506989d6eb81f673fabf
= <mmonir.m.29@gmail.com>
2023-02-28 18:25:32 +0600

    Remove unnecessery breakpoint from fill_department_designation_user_info signal.

--- department/signals.py
+++ department/signals.py
@@ -7,7 +7,6 @@ from .models import DepartmentMember
 
 @receiver(signal=pre_save, sender=DepartmentMember)
 def fill_department_designation_user_info(sender, instance, **kwargs):
-    breakpoint()
     instance.department_name = instance.department.name
     instance.designation_title = instance.designation.title
     return instance
