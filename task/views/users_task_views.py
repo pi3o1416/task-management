6a76c532ce420073f75d2c42264d992ddaed72d5
= <mmonir.m.29@gmail.com>
2023-03-02 13:28:51 +0600

    Remove  assign and retrieve method from UsersTasksViewSet. Add views AuthUsersAssignedUserTask, AuthUserAssignedUponUserTasks, DepartmentUsersTasks.

--- task/views/users_task_views.py
+++ task/views/users_task_views.py
@@ -1,52 +1,33 @@
 
 from django.contrib.auth import get_user_model
 from django.utils.translation import gettext_lazy as _
-from rest_framework.viewsets import ViewSet
-from rest_framework.views import APIView
-from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
-from rest_framework.pagination import PageNumberPagination
-from rest_framework.decorators import action
+from rest_framework.response import Response
 from rest_framework import status
 
+from services.pagination import CustomPageNumberPagination
+from services.views import TemplateViewSet, TemplateAPIView
+from department.permissions import IsSameDepartment
 from ..serializers import UsersTasksSerializers, UsersTasksDetailSerializer, UsersTasksCreateAndAssignSerializer
 from ..models import UsersTasks, Task
-from ..permissions import IsOwner
+from ..permissions import IsTaskOwner, CanViewInterDepartmentTask, CanCreateTask, CanViewAllTasks
+from ..permissions import CanCreateUsersTasks, IsUserTaskOwner
 
 
 User = get_user_model()
 
 
-class UsersTasksViewSet(ViewSet, PageNumberPagination):
-    @action(methods=['post'], detail=False, url_path='')
-    def assign(self, request):
-        """
-        Assign task to user
-        """
-        task = Task.objects.get_task_by_pk(request.data.get('task'))
-        self.check_object_permissions(request=request, obj=task)
-        serializer = self.get_serializer_class()(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
-        return Response(data={"field_errors": serializer.errors}, status=status.HTTP_400_BAD_REQUEST)
+class UsersTasksViewSet(TemplateViewSet, CustomPageNumberPagination):
+    model = UsersTasks
 
     def list(self, request):
         """
         List of all tasks
         """
-        tasks = UsersTasks.objects.filter_from_query_params(request)
+        tasks = UsersTasks.objects.select_related('task').filter_from_query_params(request)
         page = self.paginate_queryset(queryset=tasks, request=request)
         serializer = self.get_serializer_class()(instance=page, many=True)
         return self.get_paginated_response(data=serializer.data)
-
-    def retrieve(self, request, pk):
-        """
-        Detail of specific task
-        """
-        user_task = self.get_object(pk)
-        serializer = self.get_serializer_class()(instance=user_task)
-        return Response(data=serializer.data, status=status.HTTP_200_OK)
 
     def destroy(self, request, pk):
         """
@@ -56,14 +37,12 @@ class UsersTasksViewSet(ViewSet, PageNumberPagination):
         user_task.delete()
         return Response(data={"detail": _("User Task destroy successful")})
 
-    def get_object(self, pk):
-        user_task = UsersTasks.objects.get_user_task_by_pk(pk)
-        return user_task
-
     def get_permissions(self):
-        permission_classes = [IsAuthenticated]
-        if self.action == 'assign':
-            permission_classes += [IsOwner]
+        permission_classes = []
+        if self.action == 'list':
+            permission_classes += [CanViewAllTasks]
+        elif self.action == 'destroy':
+            permission_classes += [IsUserTaskOwner]
         return [permission() for permission in permission_classes]
 
     def get_serializer_class(self):
@@ -74,44 +53,99 @@ class UsersTasksViewSet(ViewSet, PageNumberPagination):
         return UsersTasksSerializers
 
 
-class UsersTasksCreateAndAssign(APIView):
+class UserTaskAssign(TemplateAPIView):
+    """
+    Assign a user previously created task.
+    """
+    model = Task
+    serializer_class = UsersTasksSerializers
+    permission_classes = [IsTaskOwner, CanCreateUsersTasks]
+
+    def post(self, request, task_pk):
+        task = self.get_object(pk=task_pk)
+        serializer = self.serializer_class(data=request.data)
+        if serializer.is_valid():
+            user_task = serializer.create(task=task)
+            response_serializer = self.serializer_class(instance=user_task)
+            return Response(data=response_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class UsersTasksCreateAndAssign(TemplateAPIView):
     """
     Create a new task and assign it to a user.
     """
     serializer_class = UsersTasksCreateAndAssignSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = [CanCreateTask]
 
     def post(self, request):
         user = request.user
         serializer = self.serializer_class(data=request.data)
         if serializer.is_valid():
             user_task = serializer.create(serializer.validated_data, user)
-            serializer = UsersTasksDetailSerializer(instance=user_task)
-            return Response(data={"detail": serializer.data}, status=status.HTTP_201_CREATED)
+            response_serializer = UsersTasksDetailSerializer(instance=user_task)
+            return Response(data=response_serializer.data, status=status.HTTP_201_CREATED)
         return Response(data={"field_errors": serializer.errors})
 
 
-class UserTasksList(APIView, PageNumberPagination):
+class UserTasksList(TemplateAPIView, CustomPageNumberPagination):
     """
     API view to get user specific task list
     """
+    model = User
+    permission_classes = [IsSameDepartment, CanViewInterDepartmentTask]
     serializer_class = UsersTasksDetailSerializer
 
     def get(self, request, user_pk):
-        user = User.objects.get_user_by_pk(user_pk)
-        user_tasks = user.user_tasks.select_related('task')
+        user = self.get_object(pk=user_pk)
+        user_tasks = user.user_tasks.select_related('task').all()
         filtered_user_tasks = user_tasks.filter_with_task(request)
         page = self.paginate_queryset(queryset=filtered_user_tasks, request=request)
         serializer = self.serializer_class(instance=page, many=True)
         return self.get_paginated_response(data=serializer.data)
 
 
+class AuthUserAssignedUserTasks(TemplateAPIView, CustomPageNumberPagination):
+    """
+    API View to get user created user tasks.
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = UsersTasksDetailSerializer
+
+    def get(self, request):
+        user = request.user
+        user_tasks = UsersTasks.objects.select_related('task').filter(task__created_by=user)\
+            .filter_from_query_params(request=request)
+        page = self.paginate_queryset(queryset=user_tasks, request=request)
+        serializer = self.serializer_class(instance=page, many=True)
+        return self.get_paginated_response(data=serializer.data)
 
 
+class AuthUserAssignedUponUserTasks(TemplateAPIView, CustomPageNumberPagination):
+    """
+    API View to get list of all user assigned upon user tasks
+    """
+    permission_classes = [IsAuthenticated]
+    serializer_class = UsersTasksDetailSerializer
+
+    def get(self, request):
+        user = request.user
+        assigned_upon_user_tasks = user.user_tasks.all().filter_from_query_params(request=request)
+        page = self.paginate_queryset(queryset=assigned_upon_user_tasks, request=request)
+        serializer = self.serializer_class(instance=page, many=True)
+        return self.get_paginated_response(data=serializer.data)
 
 
+class DepartmentUserTasks(TemplateAPIView, CustomPageNumberPagination):
+    serializer_class = UsersTasksDetailSerializer
+    permission_classes = [CanViewAllTasks|(IsSameDepartment, CanViewInterDepartmentTask)]
 
-
+    def get(self, request, department_pk):
+        department_members_task = UsersTasks.objects.department_based_tasks(department_pk)
+        filtered_tasks = department_members_task.filter_from_query_params(request=request)
+        page = self.paginate_queryset(queryset=filtered_tasks, request=request)
+        serializer = self.serializer_class(instance=page, many=True)
+        return self.get_paginated_response(data=serializer.data)
 
 
 
