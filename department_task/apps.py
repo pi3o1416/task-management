82d4b226c156d6c73f01053bff64f661698f447c
= <mmonir.m.29@gmail.com>
2023-03-05 17:23:37 +0600

    Add signal on app ready method.

--- department_task/apps.py
+++ department_task/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class DepartmentTaskConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'department_task'
+
+    def ready(self):
+        from . import signals
