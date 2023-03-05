962e723fad770ab20031519e01b8e65d8bb0e0f2
= <mmonir.m.29@gmail.com>
2023-03-05 17:18:21 +0600

    Add IsBelongToDepartment permission. This permission will be responsible to verify if requested user belong to department object.

--- department/permissions.py
+++ department/permissions.py
@@ -1,17 +1,28 @@
 
+from django.contrib.auth import get_user_model
 from rest_framework.permissions import BasePermission
 
 from services.decorators import is_authenticated
-from .models import DepartmentMember
+from .models import DepartmentMember, Department
 
 
+User = get_user_model()
 
 
 class IsSameDepartment(BasePermission):
     @is_authenticated
-    def has_object_permission(self, request, view, obj):
+    def has_object_permission(self, request, view, obj:User):
         department_members = DepartmentMember.objects.filter(member__in=[request.user.pk, obj.pk])
         if department_members.is_members_department_same():
+            return True
+        return False
+
+
+class IsBelongToDepartment(BasePermission):
+    @is_authenticated
+    def has_object_permission(self, request, view, obj:Department):
+        user = request.user
+        if user.user_department == obj:
             return True
         return False
 
