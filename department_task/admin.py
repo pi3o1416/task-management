08890097ab83688438b2c8eab86787f9b454acc7
= <mmonir.m.29@gmail.com>
2023-03-05 17:18:52 +0600

    Add get_readonly_fields method to DepartmentTaskAdmin.

--- department_task/admin.py
+++ department_task/admin.py
@@ -7,6 +7,12 @@ from .models import DepartmentTask
 class DepartmentTaskAdmin(admin.ModelAdmin):
     list_display = [field.name for field in DepartmentTask._meta.fields]
 
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ['task']
+        return self.readonly_fields
+        pass
+
 
 
 
