fa70da89aa994b1a818d08273433a524e46b6db1
= <mmonir.m.29@gmail.com>
2023-03-06 18:31:58 +0600

    Add Admin panel for Review and GoalLastEdit model.

--- goal/admin.py
+++ goal/admin.py
@@ -1,17 +1,19 @@
 
 from django.contrib import admin
-from .models import Goal
+from .models import Goal, GoalLastEdit, Review
 
 
 @admin.register(Goal)
 class GloadAdmin(admin.ModelAdmin):
-    list_display = ['department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
+    list_display = [field.name for field in Goal._meta.fields]
 
 
+@admin.register(Review)
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in Review._meta.fields]
 
 
-
-
-
-
+@admin.register(GoalLastEdit)
+class GoalLastEditAdmin(admin.ModelAdmin):
+    list_display = [field.name for field in GoalLastEdit._meta.fields]
 
