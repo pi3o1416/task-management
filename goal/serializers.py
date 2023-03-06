5ee02bef12eada915a89ab46a4c57f9e2b7108c9
= <mmonir.m.29@gmail.com>
2023-03-06 18:33:23 +0600

    Add GaolDetailSerializer to view single goal.

--- goal/serializers.py
+++ goal/serializers.py
@@ -1,7 +1,7 @@
 
 from rest_framework import serializers
 
-from .models import Goal
+from .models import Goal, Review
 
 
 class EmptySerializer(serializers.Serializer):
@@ -10,21 +10,23 @@ class EmptySerializer(serializers.Serializer):
 class GoalSerializer(serializers.ModelSerializer):
     class Meta:
         model = Goal
-        fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
-        read_only_fields = ['pk', 'review_status', 'review', 'completion', 'department']
+        fields = '__all__'
+        read_only_fields = ['pk', 'created_by', 'status', 'completion']
 
-    def create(self, commit=True):
+    def create(self, created_by, commit=True):
         assert self.validated_data != None, "Validate serializer before create goal instance"
-        goal = Goal.create_factory(commit=commit, **self.validated_data)
+        goal = Goal.create_factory(created_by=created_by, commit=commit, **self.validated_data)
         return goal
 
 
 class GoalReviewSerializer(serializers.ModelSerializer):
     review = serializers.CharField(required=True)
+
     class Meta:
         model=Goal
-        fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
-        read_only_fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'completion']
+        fields = '__all__'
+        read_only_fields = ['pk', 'created_by', 'department', 'title', 'description', 'year',
+                            'quarter', 'completion']
 
     def add_review(self, instance, validated_data):
         review = self.validated_data.get('review')
@@ -35,8 +37,9 @@ class GoalReviewSerializer(serializers.ModelSerializer):
 class GoalPercentageSerializer(serializers.ModelSerializer):
     class Meta:
         model=Goal
-        fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
-        read_only_fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review']
+        fields = '__all__'
+        read_only_fields = ['pk', 'created_by', 'department', 'title', 'description', 'year',
+                            'quarter', 'status']
 
     def update_percentage(self, instance):
         completion = self.validated_data.get('completion')
@@ -47,7 +50,38 @@ class GoalPercentageSerializer(serializers.ModelSerializer):
 class GoalUpdateSerializer(serializers.ModelSerializer):
     class Meta:
         model=Goal
-        fields = ['pk', 'department', 'title', 'description', 'year', 'quarter', 'review_status', 'review', 'completion']
-        read_only_fields = ['pk', 'department', 'review_status', 'review']
+        fields = '__all__'
+        read_only_fields = ['pk', 'created_by', 'department', 'status', 'completion']
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Review
+        fields = ['pk', 'goal', 'review', 'reviewed_by', 'reviewed_at']
+        read_only_fields = ['pk', 'reviewed_at', 'reviewed_by']
+
+
+
+
+class GoalDetailSerializer(serializers.ModelSerializer):
+    reviews = serializers.SerializerMethodField()
+    class Meta:
+        model = Goal
+        fields = ['pk', 'created_by', 'department', 'title', 'description', 'year', 'quarter',
+                  'status', 'completion', 'reviews']
+        read_only_fields = ['pk', 'created_by', 'department', 'title', 'description', 'year',
+                            'quarter', 'status', 'completion', 'reviews']
+
+    def get_reviews(self, goal):
+        goal_reviews = goal.reviews.all()
+        review_serializer = ReviewSerializer(instance=goal_reviews, many=True)
+        return review_serializer.data
+
+
+
+
+
+
+
 
 
