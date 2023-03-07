aaa11727131aa9167699426543ad87f35b247ba6
= <mmonir.m.29@gmail.com>
2023-03-07 18:20:37 +0600

    Add DepartmentTeams View and update create mehtod of TeamViewSet due to serializer create method change.

--- team/views/team_views.py
+++ team/views/team_views.py
@@ -1,26 +1,27 @@
 
 from django.utils.translation import gettext_lazy as _
 from rest_framework import status
-from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
-from rest_framework.pagination import PageNumberPagination
 from rest_framework.permissions import IsAuthenticated
 
+from services.pagination import CustomPageNumberPagination
+from services.views import TemplateViewSet, TemplateAPIView
+from department.models import Department
 from ..serializers import TeamSerializer, TeamUpdateSerializer
 from ..models import Team
 
 
-class TeamViewSet(ViewSet, PageNumberPagination):
+class TeamViewSet(TemplateViewSet, CustomPageNumberPagination):
+    model = Team
+
     def create(self, request):
-        breakpoint()
         user = request.user
         user_department = user.user_department.department
         serializer = self.get_serializer_class()(data=request.data)
         if serializer.is_valid():
-            team = serializer.create(commit=False)
-            team.department = user_department
-            team.save()
-            return Response(data=serializer.data, status=status.HTTP_201_CREATED)
+            team = serializer.create(department=user_department)
+            response_serializer = self.get_serializer_class()(instance=team)
+            return Response(data=response_serializer.data, status=status.HTTP_201_CREATED)
         return Response(data=serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     def list(self, request):
@@ -52,14 +53,22 @@ class TeamViewSet(ViewSet, PageNumberPagination):
         return [permission() for permission in permission_classes]
 
 
-    def get_object(self, pk):
-        team = Team.objects.get_object_by_pk(pk)
-        return team
-
     def get_serializer_class(self):
         if self.action == 'update':
             return TeamUpdateSerializer
         return TeamSerializer
+
+
+class DepartmentTeams(TemplateAPIView, CustomPageNumberPagination):
+    model = Department
+    serializer_class = TeamSerializer
+
+    def get(self, request, department_pk):
+        department = self.get_object(pk=department_pk)
+        department_teams = department.departmetn_teams.all()
+        page = self.paginate_queryset(queryset=department_teams, request=request)
+        serializer = self.serializer_class(instance=page, many=True)
+        return self.get_paginated_response(data=serializer.data)
 
 
 
