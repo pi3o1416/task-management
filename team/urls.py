23ca39cae7c0260f3ca2ea9e9b245797473b3cd4
= <mmonir.m.29@gmail.com>
2023-03-07 18:21:03 +0600

    Add DepartmentTeams view to urlpatterns.

--- team/urls.py
+++ team/urls.py
@@ -3,6 +3,7 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from .views import TeamViewSet, TeamTaskCreateAndAssign, TeamTasksDetail, TeamTasksList, TasksOfTeamList
+from .views import DepartmentTeams
 
 
 team_router = DefaultRouter()
@@ -15,6 +16,7 @@ urlpatterns = [
     path('tasks/<int:team_task_pk>/', TeamTasksDetail.as_view(), name='team-task-detail'),
     path('<int:team_pk>/create-and-assign-task/', TeamTaskCreateAndAssign.as_view(), name='team-task-create-assign'),
     path('<int:team_pk>/tasks/', TasksOfTeamList.as_view(), name='tasks-of-team'),
+    path('department-teams/<int:department_pk>/', DepartmentTeams.as_view(), name='department_teams'),
     path('', include(team_router.urls)),
 ]
 
