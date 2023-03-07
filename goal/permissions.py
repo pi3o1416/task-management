0e70cfd4d43153e1aef960e980e35febde41e107
= <mmonir.m.29@gmail.com>
2023-03-07 13:27:14 +0600

    Add Goal View, Change, Delete and Review add permission.

--- goal/permissions.py
+++ goal/permissions.py
@@ -3,7 +3,7 @@ from django.forms import model_to_dict
 from rest_framework.permissions import BasePermission
 
 from department.models import DepartmentMember
-from services.decorators import has_kperms
+from services.decorators import has_kperms, is_authenticated
 
 
 @has_kperms(['goal.view_goal'])
@@ -16,7 +16,33 @@ class CanCreateGoal(BasePermission):
     pass
 
 
+@has_kperms(['goal.can_view_all_goals'])
+class CanViewAllGoals(BasePermission):
+    pass
+
+
+@has_kperms(['goal.change_goal'])
+class CanChangeGoal(BasePermission):
+    pass
+
+
+@has_kperms(['goal.delete_goal'])
+class CanDeleteGoal(BasePermission):
+    pass
+
+
+@has_kperms(['goal.can_change_status'])
+class CanChangeGoalStatus(BasePermission):
+    pass
+
+
+@has_kperms(['review.add_review'])
+class CanAddReview(BasePermission):
+    pass
+
+
 class IsGoalAndUserDepartmentSame(BasePermission):
+    @is_authenticated
     def has_object_permission(self, request, view, goal):
         user = request.user
         user_department_pk = DepartmentMember.objects.member_department(member=user)
@@ -24,4 +50,6 @@ class IsGoalAndUserDepartmentSame(BasePermission):
         if user_department_pk != goal_department_pk:
             return False
         return True
+
+
 
