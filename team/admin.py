a95aeaf4aa87a8259a77c14d6897b6b2345a9213
= <mmonir.m.29@gmail.com>
2023-03-07 15:57:39 +0600

    Remove TeamMemberAdmin.

--- team/admin.py
+++ team/admin.py
@@ -1,16 +1,11 @@
 
 from django.contrib import admin
-from .models import Team, TeamMember, TeamTasks
+from .models import Team, TeamTasks
 
 
 @admin.register(Team)
 class TeamAdmin(admin.ModelAdmin):
     list_display = ['pk', 'title', 'description', 'team_lead', 'department']
-
-
-@admin.register(TeamMember)
-class TeamMemberAdmin(admin.ModelAdmin):
-    list_display = ['pk', 'team', 'member', 'member_full_name', 'member_username']
 
 
 @admin.register(TeamTasks)
