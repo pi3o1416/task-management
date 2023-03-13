59537814ef485fbf56af937b2d1246508e8fdd03
= <mmonir.m.29@gmail.com>
2023-03-13 15:58:31 +0600

    Add cache_department_info and cache_designation_info signal to Update cache on any change on model.

--- department/signals.py
+++ department/signals.py
@@ -1,8 +1,9 @@
 
-from django.db.models.signals import pre_save
+from django.db.models.signals import pre_save, post_save
 from django.dispatch import receiver
 
-from .models import DepartmentMember
+from .models import DepartmentMember, Department, Designations
+from . import tasks
 
 
 @receiver(signal=pre_save, sender=DepartmentMember)
@@ -12,12 +13,12 @@ def fill_department_designation_user_info(sender, instance, **kwargs):
     return instance
 
 
+@receiver(signal=post_save, sender=Department)
+def cache_department_info(sender, instance, **kwargs):
+    tasks.cache_department.delay()
 
 
-
-
-
-
-
-
+@receiver(signal=post_save, sender=Designations)
+def cache_designation_info(sender, instance, **kwargs):
+    tasks.cache_designation.delay()
 
