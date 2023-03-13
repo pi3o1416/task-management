fb09eaa9152e722b88cd39075127aec92d7b5cc8
= <mmonir.m.29@gmail.com>
2023-03-13 18:14:26 +0600

    Add DesignationDetailSerializer.

--- department/serializers/designation_serializers.py
+++ department/serializers/designation_serializers.py
@@ -1,6 +1,9 @@
 
+from django.forms import model_to_dict
+from django.core.cache import cache
 from rest_framework import serializers
-from ..models import Designations
+from ..models import Designations, Department
+from .department_serializers import DepartmentMinimalSerializer
 
 class DesignationSerializer(serializers.ModelSerializer):
     class Meta:
@@ -12,6 +15,19 @@ class DesignationSerializer(serializers.ModelSerializer):
         assert self.validated_data, "Call is_valid method before update"
         assert self.instance, "Initialize seirlaizer with department instance"
         self.instance.update(**self.validated_data)
+
+
+class DesignationDetailSerializer(serializers.ModelSerializer):
+    department = serializers.SerializerMethodField()
+    class Meta:
+        model = Designations
+        fields = ['pk', 'department', 'title']
+
+    def get_department(self, designation):
+        department_pk = model_to_dict(designation, fields=['department']).get('department')
+        department = Department.objects.get_object_from_cache(pk=department_pk, raise_exception=True)
+        serializer = DepartmentMinimalSerializer(instance=department)
+        return serializer.data
 
 
 class DesignationPaginatedSerializer(serializers.Serializer):
