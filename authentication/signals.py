6ceeaffada4d8af72f1353f78939f4e7a12c6f1f
= <mmonir.m.29@gmail.com>
2023-03-13 15:57:42 +0600

    Add cache_users_data signal to Cache User data on any change on model.

--- authentication/signals.py
+++ authentication/signals.py
@@ -2,12 +2,18 @@
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 from .models import CustomUser
+from . import tasks
 
 
 @receiver(signal=post_save, sender=CustomUser)
 def send_account_activation_email(sender, instance, created, **kwargs):
     if created:
         instance.send_account_active_email()
+
+
+@receiver(signal=post_save, sender=CustomUser)
+def cache_users_data(sender, instance, **kwargs):
+    tasks.cache_users_data.delay()
 
 
 
