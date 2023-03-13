5bfae8f5d80f2fbba27d6fd056461483e16825af
= <mmonir.m.29@gmail.com>
2023-03-13 16:02:01 +0600

    Add redis as cache backend.

--- tms/settings/dev.py
+++ tms/settings/dev.py
@@ -35,6 +35,18 @@ INTERNAL_IPS = [
 ]
 
 
+CACHES = {
+    'default': {
+        'BACKEND': 'django_redis.cache.RedisCache',
+        'LOCATION': 'redis://localhost:6379/1',
+        'OPTIONS': {
+            'CLIENT_CLASS': 'django_redis.client.DefaultClient'
+        },
+        'KEY_PREFIX': 'tms'
+    }
+}
+
+
 
 
 
