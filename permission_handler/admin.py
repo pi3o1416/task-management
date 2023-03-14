f85a0f01a43c725c264ba24790412af6c830d7f9
= <mmonir.m.29@gmail.com>
2023-03-14 18:24:24 +0600

    Add permission model to admin.

--- permission_handler/admin.py
+++ permission_handler/admin.py
@@ -1,4 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth.models import Permission
 
 # Register your models here.
+
+
+@admin.register(Permission)
+class PermissionAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name', 'content_type', 'codename']
+    list_filter = ['content_type__model']
+
+
+
 
