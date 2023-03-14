955d814ff1a74a2d294b023d2e46d1f7520087aa
= <mmonir.m.29@gmail.com>
2023-03-14 18:21:54 +0600

    Cache field take from Department and Designaiton cache.'

--- department/tasks.py
+++ department/tasks.py
@@ -6,14 +6,14 @@ from .models import Department, Designations
 
 @shared_task(name='cache_department')
 def cache_department():
-    departments = Department.objects.all().values('pk', 'name', 'description')
+    departments = Department.objects.all().values(*Department.CACHED_FIELDS)
     departments_data = {department['pk']: department for department in departments}
     cache.set('departments', departments_data, timeout=3600*6.5)
 
 
 @shared_task(name='cache_designation')
 def cache_designation():
-    designations = Designations.objects.all().values('pk', 'department', 'title')
+    designations = Designations.objects.all().values(*Designations.CACHED_FIELDS)
     designations_data = {designation['pk']: designation for designation in designations}
     cache.set('designations', designations_data, timeout=3600*6.5)
 
