66979485095c09049ebf3d99996cd4a8cc49d442
= <mmonir.m.29@gmail.com>
2023-03-15 18:33:18 +0600

    Remove update_task_type_to_user_task, update_task_type_to_undetermine signal.

--- task/signals.py
+++ task/signals.py
@@ -3,25 +3,6 @@ from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from .models import TaskTree, UsersTasks, Task
 
-@receiver(signal=post_save, sender=UsersTasks)
-def update_task_type_to_user_task(sender, instance:UsersTasks, created, **kwargs):
-    if created:
-        task = instance.task
-        if task.task_type != Task.TaskType.USER_TASK:
-            task.update(
-                task_type = Task.TaskType.USER_TASK,
-                is_assigned = True,
-                commit=True
-            )
-
-@receiver(signal=post_delete, sender=UsersTasks)
-def update_task_type_to_undetermine(sender, instance:UsersTasks, **kwargs):
-    task = instance.task
-    task.update(
-        task_type = Task.TaskType.NONE,
-        is_assigned = False,
-        commit=True
-    )
 
 @receiver(signal=post_save, sender=TaskTree)
 def change_has_subtask_true(sender, instance:TaskTree, created, **kwargs):
