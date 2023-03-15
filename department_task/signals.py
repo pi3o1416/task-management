7e07f2764edb47f9f2a63f513d24393028a33892
= <mmonir.m.29@gmail.com>
2023-03-15 18:28:36 +0600

    Add update_task_type_to_user_task signal.

--- department_task/signals.py
+++ department_task/signals.py
@@ -1,4 +1,5 @@
 
+from django.forms import model_to_dict
 from django.dispatch import receiver
 from django.db.models.signals import post_save, post_delete
 from task.models import Task
@@ -8,14 +9,14 @@ from .models import DepartmentTask
 @receiver(signal=post_save, sender=DepartmentTask)
 def update_task_type_to_dept_task(sender, instance:DepartmentTask, created, **kwargs):
     if created:
-        task = instance.task
-        task.update(task_type=Task.TaskType.DEPARTMENT_TASK, is_assigned=True)
+        task_pk = model_to_dict(instance=instance, fields=['task'])
+        Task.objects.filter(pk=task_pk).update(task_type=Task.TaskType.DEPARTMENT_TASK)
 
 
 @receiver(signal=post_delete, sender=DepartmentTask)
-def update_task_type_to_undermine(sender, instance:DepartmentTask, **kwargs):
-    task = instance.task
-    task.update(task_type=Task.TaskType.NONE, is_assigned=False)
+def update_task_type_to_user_task(sender, instance:DepartmentTask, **kwargs):
+    task_pk = model_to_dict(instance=instance, fields=['task'])
+    Task.objects.filter(pk=task_pk).update(task_type=Task.TaskType.USER_TASK)
 
 
 
