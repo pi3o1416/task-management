7c3f580ff66b74d565d7278b2b358e2e81ac0e57
= <mmonir.m.29@gmail.com>
2023-03-15 18:33:59 +0600

    Add update_task_type_team_task update_task_type_user_task signal.

--- team/signals.py
+++ team/signals.py
@@ -1,10 +1,11 @@
 
 from django.forms import model_to_dict
 from django.dispatch import receiver
-from django.db.models.signals import post_save, m2m_changed
+from django.db.models.signals import post_save, m2m_changed, post_delete
 
+from .models import Task
 from .exceptions import TeamLeadDeleteProhabited
-from .models import Team
+from .models import Team, TeamTasks
 from . import tasks
 
 
@@ -27,3 +28,16 @@ def protect_team_lead_delete(sender, instance, action, pk_set, **kwargs):
 @receiver(signal=post_save, sender=Team)
 def cache_team_data(sender, instance, **kwargs):
     tasks.cache_teams.delay()
+
+
+@receiver(signal=post_save, sender=TeamTasks)
+def update_task_type_team_task(sender, instance, created, **kwargs):
+    if created:
+        task_pk = model_to_dict(instance=instance, fields=['task']).get('task')
+        Task.objects.filter(pk=task_pk).update(task_type=Task.TaskType.TEAM_TASK)
+
+@receiver(signal=post_delete, sender=TeamTasks)
+def update_task_type_user_task(sender, instance, **kwargs):
+    task_pk = model_to_dict(instance=instance, fields=['task']).get('task')
+    Task.objects.filter(pk=task_pk).update(task_type=Task.TaskType.USER_TASK)
+
