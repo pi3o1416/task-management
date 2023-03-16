7826b88e4cd82f323573867d2f82c18699caa82e
= <mmonir.m.29@gmail.com>
2023-03-16 19:08:20 +0600

    Remove model_to_dict function.

--- team/signals.py
+++ team/signals.py
@@ -1,5 +1,4 @@
 
-from django.forms import model_to_dict
 from django.dispatch import receiver
 from django.db.models.signals import post_save, m2m_changed, post_delete
 
@@ -20,8 +19,8 @@ def add_team_lead_as_team_member(sender, instance:Team, update_fields, **kwargs)
 @receiver(signal=m2m_changed, sender=Team.members.through)
 def protect_team_lead_delete(sender, instance, action, pk_set, **kwargs):
     if action == 'pre_remove':
-        team_lead = model_to_dict(instance).get('team_lead')
-        if team_lead in pk_set:
+        team_lead_id = instance.team_lead_id
+        if team_lead_id in pk_set:
             raise TeamLeadDeleteProhabited()
 
 
@@ -33,11 +32,11 @@ def cache_team_data(sender, instance, **kwargs):
 @receiver(signal=post_save, sender=TeamTasks)
 def update_task_type_team_task(sender, instance, created, **kwargs):
     if created:
-        task_pk = model_to_dict(instance=instance, fields=['task']).get('task')
+        task_pk = instance.task_id
         Task.objects.filter(pk=task_pk).update(task_type=Task.TaskType.TEAM_TASK)
 
 @receiver(signal=post_delete, sender=TeamTasks)
 def update_task_type_user_task(sender, instance, **kwargs):
-    task_pk = model_to_dict(instance=instance, fields=['task']).get('task')
+    task_pk = instance.task_id
     Task.objects.filter(pk=task_pk).update(task_type=Task.TaskType.USER_TASK)
 
