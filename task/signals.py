68141d6ac0a648f5cd80723d7f5a6eec6a167f64
Monir Hossain <mmonir.m.29@gmail.com>
2023-03-19 01:36:20 +0600

    Add signal make_task_approved_by_default.

--- task/signals.py
+++ task/signals.py
@@ -1,6 +1,6 @@
 
 from django.dispatch import receiver
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_save
 from .models import TaskTree, UsersTasks, Task
 
 
@@ -22,13 +22,11 @@ def change_has_subtask_false(sender, instance:TaskTree, **kwargs):
             parent.update(has_subtask=True, commit=True)
 
 
-
-
-
-
-
-
-
-
-
+@receiver(signal=pre_save, sender=Task)
+def make_task_approved_by_default(sender, instance:Task, **kwargs):
+    if instance.pk == None:
+        created_by = instance.created_by
+        if created_by.has_perm("task.can_approve_disapprove_task"):
+            instance.approval_status = Task.ApprovalChoices.APPROVED
+    return instance
 
