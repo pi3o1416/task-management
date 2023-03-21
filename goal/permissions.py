cc4c5dee6719fcacfaaeb1cf8a2189e87e499aa8
= <mmonir.m.29@gmail.com>
2023-03-21 16:05:11 +0600

    Add Goal permissions messages.

--- goal/permissions.py
+++ goal/permissions.py
@@ -8,40 +8,42 @@ from services.decorators import has_kperms, is_authenticated
 
 @has_kperms(['goal.view_goal'])
 class CanViewGoal(BasePermission):
-    pass
+    message = 'You do not have permission to view goals'
 
 
 @has_kperms(['goal.add_goal'])
 class CanCreateGoal(BasePermission):
-    pass
+    message = 'You do not have permission to create goal'
 
 
 @has_kperms(['goal.can_view_all_goals'])
 class CanViewAllGoals(BasePermission):
-    pass
+    message = 'You do not have permission to view all goals'
 
 
 @has_kperms(['goal.change_goal'])
 class CanChangeGoal(BasePermission):
-    pass
+    message = 'You do not have permission to update goals'
 
 
 @has_kperms(['goal.delete_goal'])
 class CanDeleteGoal(BasePermission):
-    pass
+    message = 'You do not have permission to delete goals'
 
 
 @has_kperms(['goal.can_change_status'])
 class CanChangeGoalStatus(BasePermission):
-    pass
+    message = 'You do not have permission to update goal status'
 
 
 @has_kperms(['review.add_review'])
 class CanAddReview(BasePermission):
+    message = 'You do not have permission to add goal review'
     pass
 
 
 class IsGoalAndUserDepartmentSame(BasePermission):
+    message = 'Goal department and requested user department is not same'
     @is_authenticated
     def has_object_permission(self, request, view, goal):
         user = request.user
