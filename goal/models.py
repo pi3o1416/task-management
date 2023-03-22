2ff6b9e6331eb6bd88eae710f300f8e8a11c206b
= <mmonir.m.29@gmail.com>
2023-03-22 13:13:05 +0600

    :pencil2: Fix permission codename for can_view_all_goals.

--- goal/models.py
+++ goal/models.py
@@ -74,7 +74,7 @@ class Goal(ModelUpdateMixin, models.Model):
     objects = GoalQuerySet().as_manager()
     class Meta:
         permissions = (("can_change_status", _("Can Change Status")),
-                       ("cna_view_all_goals", _("Can View All Goals")),)
+                       ("can_view_all_goals", _("Can View All Goals")),)
 
     def clean(self, update_fields=None):
         #Validate rejected goals can not be updated
