40131fe46aa585acfa8bfff41f29c4f8ca0915de
Monir Hossain <mmonir.m.29@gmail.com>
2023-04-08 23:15:57 +0600

    Remove sentry config.

--- tms/settings/dev.py
+++ tms/settings/dev.py
@@ -1,9 +1,4 @@
 
-<<<<<<< HEAD
-=======
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
->>>>>>> 067b979 (Configure sentry support.)
 from .base import *
 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -26,28 +21,5 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_RESULT_EXTENDED = True
+CELERY_BROKER_URL = 'amqp://user:bitnami@localhost:5672/'
 
-<<<<<<< HEAD
-
-
-
-
-
-=======
-#Configuration for sentry
-sentry_sdk.init(
-    dsn="https://a1ff75fa9a95483693c31dea9957c4f3@o1182560.ingest.sentry.io/4504327056850944",
-    integrations=[
-        DjangoIntegration(),
-    ],
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True
-)
->>>>>>> 067b979 (Configure sentry support.)
