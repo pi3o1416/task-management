9d3da23919d53001eb37e05aefb1f84e148cf5e7
Monir Hossain <mmonir.m.29@gmail.com>
2023-04-08 23:18:58 +0600

    Replace rabbitmq with redis.

--- tms/settings/dev.py
+++ tms/settings/dev.py
@@ -21,7 +21,7 @@ CELERY_TASK_TIME_LIMIT = 30 * 60
 CELERY_RESULT_BACKEND = 'django-db'
 CELERY_CACHE_BACKEND = 'django-cache'
 CELERY_RESULT_EXTENDED = True
-CELERY_BROKER_URL = 'amqp://user:bitnami@tmsv2_rabbitmq:5672/'
+CELERY_BROKER_URL = 'redis://localhost:6379/'
 
 
 
